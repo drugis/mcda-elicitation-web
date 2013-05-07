e5fc60e0d29d5c583aa5dff282b17125f5963d60
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-05-07 17:24:56 +0200

    Configureable examples repository URL

--- js/elicit/DecisionProblem.js
+++ js/elicit/DecisionProblem.js
@@ -1,7 +1,8 @@
 angular.module('elicit.example', ['ngResource']).factory('DecisionProblem', function($resource, $rootScope) {
+  var repositoryUrl = config ? config.examplesRepository : '';
   var Problem = {
     url: undefined,
-    resource: $resource('examples/:url', {url:'@url'}),
+    resource: $resource(repositoryUrl + ':url', {url:'@url'}),
     get: function(callback) {
       if (this.url) {
         return this.resource.get({url: this.url}, callback);
