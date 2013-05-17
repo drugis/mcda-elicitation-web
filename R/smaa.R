4a554b264cf4f34b7d40305b39c891ac3625438b
Joël Kuiper <me@joelkuiper.eu>
2013-05-17 12:42:51 +0200

    Add smaa R file to run with clinicico
    Recommended way to start the worker is with 
    
    `lein run --file YOURPATHTO/R/smaa.R --method smaa --packages "rsmaa, hitandrun, rcdd"`

---
+++ R/smaa.R
@@ -0,0 +1,91 @@
+library(smaa)
+library(hitandrun)
+
+wrap.result <- function(result, description) {
+    list(data=result, description=description, type=class(result))
+}
+harSample <- function(constr, n , N) {
+    transform <- simplex.createTransform(n)
+    constr <- simplex.createConstraints(transform, constr)
+    seedPoint <- createSeedPoint(constr, homogeneous=TRUE)
+    har(seedPoint, constr, N=N * (n-1)^3, thin=(n-1)^3, homogeneous=TRUE, transform=transform)$samples
+}
+
+partialValue <- function(worst, best) {
+  if (best > worst) {
+    function(x) {
+      (x - worst) / (best - worst)
+    }
+  } else {
+    function(x) {
+      (worst - x) / (worst - best)
+    }
+  }
+}
+
+smaa <- function(params) {
+  N <- 10000
+  n <- length(params$criteria)
+  m <- length(params$alternatives)
+  crit <- names(params$criteria)
+  alts <- names(params$alternatives)
+
+  pvf <- lapply(params$criteria, function(criterion) {
+    range <- criterion$pvf$range
+    if (criterion$pvf$type == 'linear-increasing') {
+      return(partialValue(range[1], range[2]))
+    } else if (criterion$pvf$type == 'linear-decreasing') {
+      return(partialValue(range[2], range[1]))
+    } else {
+      stop(paste("PVF type '", criterion$pvf$type, "' not supported.", sep=''))
+    }
+  })
+
+  meas <- array(dim=c(N,m,n), dimnames=list(NULL, alts, crit))
+  for (m in params$performanceTable) {
+    if (m$performance$type == 'dbeta') {
+      meas[, m$alternative, m$criterion] <- pvf[[m$criterion]](rbeta(N, m$performance$parameters['alpha'], m$performance$parameters['beta']))
+    } else {
+      stop(paste("Performance type '", m$performance$type, "' not supported.", sep=''))
+    }
+  }
+
+  # parse preference information
+  constr <- do.call(mergeConstraints, lapply(params$preferences,
+    function(statement) {
+      i1 <- which(crit == statement$criteria[1])
+      i2 <- which(crit == statement$criteria[2])
+      if (statement$type == "ordinal") {
+        ordinalConstraint(n, i1, i2)
+      } else if (statement['type'] == "ratio bound") {
+        l <- statement$bounds[1]
+        u <- statement$bounds[2]
+        mergeConstraints(
+          lowerRatioConstraint(n, i1, i2, l),
+          upperRatioConstraint(n, i1, i2, u)
+        )
+      }
+    })
+  )
+
+  weights <- harSample(constr, n, N)
+
+  utils <- smaa.values(meas, weights)
+  ranks <- smaa.ranks(utils)
+
+  wrap.matrix <- function(m) {
+      l <- lapply(rownames(m), function(name) { m[name,] })
+      names(l) <- rownames(m)
+      l
+  }
+  results <- list(
+    results = list("parameters"=params,
+             "ranks"=wrap.matrix(smaa.ra(ranks))),
+    descriptions = list("Parameters", "Rank acceptabilities")
+  )
+
+  mapply(wrap.result,
+          results$results,
+          results$descriptions,
+          SIMPLIFY=F)
+}
