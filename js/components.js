90de3311aa0972b99dad1f08ddc834184c78b223
Joël Kuiper <me@joelkuiper.eu>
2013-06-17 13:40:17 +0200

    Use nvd3 instead of custom plotting

--- js/components.js
+++ js/components.js
@@ -62,8 +62,8 @@ angular.module('elicit.components', []).
 }).
   directive('barChart', function() {
   var margin = {top: 10, right: 20, bottom: 20, left: 60},
-  width = 300 - margin.left - margin.right,
-  height = 480 - margin.top - margin.bottom;
+  width = 350 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
   return {
     restrict:'E',
     scope: {
@@ -72,117 +72,39 @@ angular.module('elicit.components', []).
     },
     link: function(scope, element, attrs) {
       function parseData(data) {
-        var result = _.map(_.pairs(data), function(el) {
+        var result = [];
+        _.each(_.pairs(data), function(el) {
           var key = el[0];
           var values = el[1];
-          return _.map(_.pairs(values),
-                       function(el) {
-                         return { intervention: key, group: el[0], value: el[1] }
-                       });
+          for(var i = 0; i < values.length; i++) {
+            var obj = result[i] || { key: "Rank " + (i + 1), values: [] };
+            obj.values.push({x: key, y: values[i]});
+            result[i] = obj;
+          }
         });
-        return _.flatten(result);
+        return result;
       }
-
-      var y0 = d3.scale.ordinal()
-      .rangeRoundBands([height, 0], .2);
-
-      var y1 = d3.scale.linear();
-
-      var x = d3.scale.ordinal()
-      .rangeRoundBands([0, width], .1, 0);
-
-      var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-
-      var nest = d3.nest()
-      .key(function(d) { return d.group; });
-
-      var stack = d3.layout.stack()
-      .values(function(d) { return d.values; })
-      .x(function(d) { return d.intervention; })
-      .y(function(d) { return d.value; })
-      .out(function(d, y0) { d.valueOffset = y0; });
-
-      var color = d3.scale.category20c();
 
       var svg = d3.select(element[0]).append("svg")
       .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+      .attr("height", height + margin.top + margin.bottom);
       scope.$watch('val', function(newVal, oldVal) {
-        svg.selectAll('*').remove();
         if(!newVal) return;
+        nv.addGraph(function() {
+          var chart = nv.models.multiBarChart();
+          var data = parseData(newVal);
 
-        var data = parseData(newVal);
-        var dataByGroup = nest.entries(data);
+          chart.yAxis
+            .tickFormat(d3.format(',.3f'));
 
-        stack(dataByGroup);
-        x.domain(dataByGroup[0].values.map(function(d) { return d.intervention; }));
-        y0.domain(dataByGroup.map(function(d) { return d.key; }));
-        y1.domain([0, d3.max(data, function(d) { return d.value; })]).range([y0.rangeBand(), 0]);
+          svg.datum(data)
+            .transition().duration(100).call(chart);
 
-        var group = svg.selectAll(".group")
-        .data(dataByGroup)
-        .enter().append("g")
-        .attr("class", "group")
-        .attr("transform", function(d) { return "translate(0," + y0(d.key) + ")"; });
+          nv.utils.windowResize(chart.update);
 
-        group.append("text")
-        .attr("class", "group-label")
-        .attr("x", -6)
-        .attr("y", function(d) { return y1(d.values[0].value / 2); })
-        .attr("dy", ".35em")
-        .text(function(d) {
-          if(d.key == 1) {
-            return "Best"
-          } else if(d.key == dataByGroup.length) {
-            return "Worst"
-          } else {
-            return null;
-          }
+          return chart;
         });
-
-        var bars = group.selectAll("g.bar")
-        .data(function(d) { return d.values; })
-        .enter().append("g")
-        .attr("class", "bar");
-
-        bars.append("rect")
-        .style("fill", function(d) { return color(d.group); })
-        .attr("x", function(d) { return x(d.intervention); })
-        .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y1(d.value); })
-        .attr("height", function(d) { return y0.rangeBand() - y1(d.value); });
-
-
-        group.filter(function(d, i) { return !i; }).append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + y0.rangeBand() + ")")
-        .call(xAxis);
       });
-
-      scope.$watch('stacked', function(newVal, oldVal) {
-        if(newVal === oldVal) return;
-        if (newVal === "multiples") transitionMultiples();
-        else transitionStacked();
-      });
-
-      function transitionMultiples() {
-        var t = svg.transition().duration(750),
-        g = t.selectAll(".group").attr("transform", function(d) { return "translate(0," + y0(d.key) + ")"; });
-        g.selectAll("rect").attr("y", function(d) { return y1(d.value); });
-        g.select(".group-label").attr("y", function(d) { return y1(d.values[0].value / 2); })
-      }
-
-      function transitionStacked() {
-        var t = svg.transition().duration(750),
-        g = t.selectAll(".group").attr("transform", "translate(0," + y0(y0.domain()[0]) + ")");
-        g.selectAll("rect").attr("y", function(d) { return y1(d.value + d.valueOffset); });
-        g.select(".group-label").attr("y", function(d) { return y1(d.values[0].value / 2 + d.values[0].valueOffset); })
-      }
     }
   }
 }).
