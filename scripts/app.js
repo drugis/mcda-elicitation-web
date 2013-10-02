9870cfbb2fa445ea34845407ecb44bdebe4f48dc
Daan Reid <osmosisch@gmail.com>
2013-10-02 15:08:27 +0200

    Move String hashCode to main.js

--- scripts/app.js
+++ scripts/app.js
@@ -4,19 +4,8 @@ define(['angular',
     'components',
     'services'],
 function(angular) {
-  var app = angular.module('elicit', ['elicit.controller', 'elicit.problem-resource', 'elicit.components', 'elicit.services']);
-
-  // Add string HashCode
-  String.prototype.hashCode = function(){
-    var hash = 0;
-    if (this.length == 0) return hash;
-    for (var i = 0; i < this.length; i++) {
-      var character = this.charCodeAt(i);
-      hash = ((hash<<5)-hash)+character;
-      hash = hash & hash; // Convert to 32bit integer
-    }
-    return hash;
-  }
+  var app = angular.module('elicit', 
+  ['elicit.controller', 'elicit.problem-resource', 'elicit.components', 'elicit.services']);
 
   app.run(['$rootScope', function($rootScope) {
     $rootScope.$safeApply = function($scope, fn) {
