ecd4085dfc3eb00284550e1f7ec651c9c1d9f7a7
Connor Stroomberg <c.stroomberg@gmail.com>
2014-01-31 15:38:43 +0100

    login integration test, as example of PageObject pattern

---
+++ test/protractor/loginStory.js
@@ -0,0 +1,16 @@
+describe('drugis login story', function() {
+
+  var ptor;
+  var LoginPage= require('./loginPage.js');
+
+  beforeEach(function() {
+     ptor = protractor.getInstance();
+  });
+
+  it('should go to chooseproblemPage after login', function() {
+    var url = ptor.params.url || 'http://localhost:8080';
+    var loginPage = new LoginPage(ptor, url);
+    var chooseProblemPage = loginPage.login();
+    expect(chooseProblemPage.getCurrentUrl()).toEqual(url + '/#/choose-problem');
+  });
+});
