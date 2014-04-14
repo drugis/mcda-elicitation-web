c3af1fd88ee19c08b9ed7a2eeaf04cd6fd87ec62
connor.stroomberg <c.stroomberg@gmail.com>
2014-04-14 22:20:13 +0200

    test for remoteWorkspace get function

--- test/unit/remoteWorkspaceSpec.js
+++ test/unit/remoteWorkspaceSpec.js
@@ -1,12 +1,41 @@
-define(['angular', 'angular-mocks', 'services/remoteWorkspaces'], function(angular, RemoteWorkspaces) {
+define(['angular', 'angular-mocks', 'services/partialValueFunction', 'services/remoteWorkspaces'], function(angular, RemoteWorkspaces) {
   describe('remote workspace init test', function() {
 
+    beforeEach(module('elicit.pvfService'));
     beforeEach(module('elicit.remoteWorkspaces'));
 
     it('should expose a set of resource functions', inject(function(RemoteWorkspaces) {
-     expect(RemoteWorkspaces.create).toBeDefined();
-     expect(RemoteWorkspaces.get).toBeDefined();
-     expect(RemoteWorkspaces.query).toBeDefined();
+      expect(RemoteWorkspaces.create).toBeDefined();
+      expect(RemoteWorkspaces.get).toBeDefined();
+      expect(RemoteWorkspaces.query).toBeDefined();
     }));
+
+  });
+
+  describe('remote workspace get test', function() {
+
+    beforeEach(module('elicit.remoteWorkspaces'));
+
+    it('should get the decorated workspace', inject(function($rootScope, $httpBackend, RemoteWorkspaces) {
+      var workspaceId = 1,
+        mockWorkspace = jasmine.createSpyObj('mockWorkspace', ['test']),
+        resolvedValue,
+        getPromise;
+
+      $httpBackend.when('GET', 'undefined1').respond(mockWorkspace);
+
+      getPromise = RemoteWorkspaces.get(workspaceId);
+
+      getPromise.then(function(value){ resolvedValue = value; });
+      $httpBackend.flush();
+      $rootScope.$apply();
+
+      // check to see is resolved workspace has been decorated as expected
+      expect(typeof resolvedValue.redirectToDefaultView).toBe('function');
+      expect(typeof resolvedValue.getScenario).toBe('function');
+      expect(typeof resolvedValue.newScenario).toBe('function');
+      expect(typeof resolvedValue.query).toBe('function');
+    }));
+
   });
 });
