28e4353f5f02cb0aba7cbbbd7de472430cd0f6dd
Connor Stroomberg <c.stroomberg@gmail.com>
2014-04-17 15:30:02 +0200

    Unhide hashcode on string prorotype by placing in service

---
+++ app/js/services/hashCodeService.js
@@ -0,0 +1,13 @@
+define([], function() {
+  // Add string HashCode
+  String.prototype.hashCode = function() {
+    var hash = 0;
+    if (this.length === 0) return hash;
+    for (var i = 0; i < this.length; i++) {
+      var character = this.charCodeAt(i);
+      hash = ((hash << 5) - hash) + character;
+      hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+  };
+});
