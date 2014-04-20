1cd8012eaf0b4f251c6dcc11f942f619a4117ff2
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2014-04-20 13:07:33 +0200

    Remove tomcat dockerfiles

--- tomcat/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -e MCDAWEB_CONTAINER ]; then echo "No container running?"; exit; fi
-
-docker stop `cat MCDAWEB_CONTAINER`
-docker commit `cat MCDAWEB_CONTAINER` mcdaweb
-rm MCDAWEB_CONTAINER
--- tomcat/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -e MCDAWEB_CONTAINER ]; then echo "Stop and Commit the container first"; exit; fi
-
-cp ../spring/target/mcda-web.war update/
-cp ../spring/database.sql update/
-docker tag mcdaweb:latest mcdaweb:previous
-cd update
-docker build -t=mcdaweb .
