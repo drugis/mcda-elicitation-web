5e508c8a4606fb397861509b0a9dc5fbaf0a44e1
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2014-04-20 11:35:12 +0200

    Split up workspace and scenario routes
     - to ease integration in addis-core
     - split up WorkspaceController/ScenarioController

--- app/js/controllers/workspace.js
+++ app/js/controllers/workspace.js
@@ -1,43 +1,7 @@
 define(['angular', 'underscore', 'mcda/config'], function(angular, _, Config) {
 
-  return function($scope, $location, $state, Tasks, TaskDependencies, currentWorkspace, currentScenario) {
+  return function($scope, $location, $state, Tasks, TaskDependencies, currentWorkspace) {
     $scope.workspace = currentWorkspace;
-    $scope.scenario = currentScenario;
-
-    $scope.scenarios = currentWorkspace.query();
-
-    var resultsAccessible = function() {
-      var resultsTask = _.find(Tasks.available, function(task) {
-        return task.id === "results";
-      });
-      var accessible = TaskDependencies.isAccessible(resultsTask, currentScenario.state);
-      return accessible.accessible;
-    };
-
-    $scope.resultsAccessible = resultsAccessible();
-
-    $scope.$on("elicit.scenariosChanged", function(e, val) {
-      $scope.scenarios = currentWorkspace.query();
-      $scope.resultsAccessible = resultsAccessible();
-    });
-
-    var redirect = function(scenarioId) {
-      $state.go(Config.defaultView, {scenarioId: scenarioId});
-    };
-
-    $scope.forkScenario = function() {
-      currentWorkspace
-        .newScenario(currentScenario.state)
-        .then(redirect);
-    };
-
-    $scope.newScenario = function() {
-      currentWorkspace
-        .newScenario({
-          "problem": currentWorkspace.problem
-        })
-        .then(redirect);
-    };
 
     $scope.isEditTitleVisible = false;
 
