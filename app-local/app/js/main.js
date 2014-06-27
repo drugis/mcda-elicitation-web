87b1565c9a061164e554e8bc1d15c544a310a3c4
Daan Reid <osmosisch@gmail.com>
2014-06-27 17:07:55 +0200

    Fixes to the bower configuration

--- app-local/app/js/main.js
+++ app-local/app/js/main.js
@@ -13,6 +13,8 @@ require.config({
     'nvd3': 'bower_components/nvd3/nv.d3.min',
     'MathJax': 'bower_components/MathJax/MathJax.js?config=TeX-MML-AM_HTMLorMML',
     'foundation': 'bower_components/foundation/js/foundation.min',
+    'mmfoundation': 'bower_components/angular-foundation/mm-foundation',
+    'angularanimate': 'bower_components/angular-animate/angular-animate.min',
     'domReady': 'bower_components/requirejs-domready/domReady',
     'mcda': 'bower_components/mcda-web/app/js'
   },
@@ -21,6 +23,7 @@ require.config({
     'angular': { exports : 'angular' },
     'angular-resource': { deps:['angular'], exports: 'angular-resource' },
     'angular-ui-router': { deps:['angular'] },
+    'angularanimate': {deps: ['angular']},
     'underscore': { exports : '_' },
     'MathJax' : { exports: 'MathJax' },
     'd3': { exports : 'd3' },
@@ -28,7 +31,8 @@ require.config({
     'jQuery': { exports : 'jQuery' },
     'jquery-slider': { deps: ['jQuery'] },
     'NProgress': { deps: ['jQuery'], exports: "NProgress" },
-    'foundation':  { deps: ['jQuery'] }
+    'foundation':  { deps: ['jQuery'] },
+    'mmfoundation': { deps: ['angular'] }
   },
   priority: ['angular']
 });
