d589cf43724e47158ea428aad1ec1cb71b130ccf
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2014-08-29 17:09:41 +0200

    Get mcda-web to load

--- app-remote/public/main.js
+++ app-remote/public/main.js
@@ -8,7 +8,11 @@ require.config({
     'angular-resource': 'bower_components/angular-resource/angular-resource.min',
     'angular-ui-router': 'bower_components/angular-ui-router/release/angular-ui-router.min',
     'foundation': 'bower_components/foundation/js/foundation.min',
-    'domReady': 'bower_components/requirejs-domready/domReady'
+    'domReady': 'bower_components/requirejs-domready/domReady',
+    'mcda': 'bower_components/mcda-web/app/js',
+    'NProgress': 'bower_components/nprogress/nprogress',
+    'd3': 'bower_components/d3/d3.min',
+    'nvd3': 'bower_components/nvd3/nv.d3.min'
   },
   baseUrl: '.',
   shim: {
@@ -24,7 +28,7 @@ require.config({
 
 window.name = "NG_DEFER_BOOTSTRAP!";
 
-require(['require', 'angular', 'temp'], function (require, angular) {
+require(['require', 'angular', 'temp', 'mcda/mcda-web'], function (require, angular) {
   require(['domReady!'], function (document) {
     angular.bootstrap(document , ['temp']);
   });
--- app-remote/public/temp.js
+++ app-remote/public/temp.js
@@ -1,10 +1,31 @@
 'use strict';
 define(
   ['angular',
+'mcda/config',
    'angular-resource',
-   'angular-ui-router'],
-  function(angular) {
-    var app = angular.module("temp", ['ui.router', 'ngResource']);
+   'angular-ui-router',
+    'mcda/controllers',
+    'mcda/controllers',
+    'mcda/directives',
+    'mcda/filters',
+    'mcda/services/remoteWorkspaces',
+    'mcda/services/taskDependencies',
+    'mcda/services/errorHandling',
+    'mcda/services/hashCodeService'
+   ],
+  function(angular, Config) {
+    var mcdaDependencies = [
+      'elicit.remoteWorkspaces',
+      'elicit.directives',
+      'elicit.filters',
+      'elicit.controllers',
+      'elicit.taskDependencies',
+      'elicit.errorHandling'
+    ];
+
+    var app = angular.module("temp", ['ui.router', 'ngResource'].concat(mcdaDependencies));
+    app.constant('Tasks', Config.tasks);
+    app.constant('DEFAULT_VIEW', 'overview');
 
     app.run(['$rootScope', function($rootScope) {
       $rootScope.$on('error', function(e, message) {
@@ -14,8 +35,11 @@ define(
       });
     }]);
 
-    app.config(function($stateProvider, $urlRouterProvider) {
+    app.config(function($stateProvider, $urlRouterProvider, Tasks) {
       $urlRouterProvider.otherwise('');
+      var baseTemplatePath = 'bower_components/mcda-web/app/views/';
+
+      //$httpProvider.interceptors.push('ErrorHandling');
 
       $stateProvider.state('root', {
         url: '',
@@ -37,6 +61,54 @@ define(
           $scope.user = user;
         }
       });
+      $stateProvider.state("workspace", {
+        parent: 'root',
+        url: '/workspaces/:workspaceId',
+        templateUrl: baseTemplatePath + 'workspace.html',
+        resolve: {
+          currentWorkspace: ["$stateParams", config.workspacesRepository.service, function($stateParams, Workspaces) {
+            return Workspaces.get($stateParams.workspaceId);
+          }]
+        },
+        controller: 'WorkspaceController',
+        abstract: true
+      })
+      .state("workspace.scenario", {
+        url: '/scenarios/:scenarioId',
+        templateUrl: baseTemplatePath + 'scenario.html',
+        resolve: {
+          currentScenario: function($stateParams, currentWorkspace) {
+            return currentWorkspace.getScenario($stateParams.scenarioId);
+          }
+        },
+        controller: 'ScenarioController'
+      });
+
+
+      _.each(Tasks.available, function(task) {
+        var templateUrl = baseTemplatePath + task.templateUrl;
+        $stateProvider.state(task.id, {
+          parent: 'workspace.scenario',
+          url: '/' + task.id,
+          templateUrl: templateUrl,
+          controller: task.controller,
+          resolve : {
+            taskDefinition: function(currentScenario, TaskDependencies) {
+              var def = TaskDependencies.extendTaskDefinition(task);
+              return def;
+            }
+          }
+        });
+      });
+
+      // Default route
+      $stateProvider.state('choose-problem',
+                           { parent: 'root',
+                             url: '/choose-problem',
+                             templateUrl: baseTemplatePath + 'chooseProblem.html',
+                             controller: "ChooseProblemController" });
+      $urlRouterProvider.otherwise('/choose-problem');
+
     });
 
     return app;
