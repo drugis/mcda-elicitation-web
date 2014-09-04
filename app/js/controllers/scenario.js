5de99bfe5d16f5b9883478c1c959e90623a8b389
Daan Reid <osmosisch@gmail.com>
2014-09-04 10:27:55 +0200

    Fix scenario select

--- app/js/controllers/scenario.js
+++ app/js/controllers/scenario.js
@@ -1,8 +1,13 @@
 define(['angular', 'underscore', 'mcda/config'], function(angular, _, Config) {
   return function($scope, $location, $state, Tasks, TaskDependencies, currentWorkspace, currentScenario) {
     $scope.workspace = currentWorkspace;
-    $scope.scenario = currentScenario;
     $scope.scenarios = currentWorkspace.query();
+
+    $scope.scenarios.$promise.then(function(scenarios) {
+      $scope.scenario = _.find($scope.scenarios, function(scenario) {
+        return scenario.id = currentScenario.id;
+      });
+    });
 
     var resultsAccessible = function() {
       var resultsTask = _.find(Tasks.available, function(task) {
@@ -20,7 +25,9 @@ define(['angular', 'underscore', 'mcda/config'], function(angular, _, Config) {
     });
 
     var redirect = function(scenarioId) {
-      $state.go(Config.defaultView, {scenarioId: scenarioId});
+      $state.go(Config.defaultView, {
+        scenarioId: scenarioId
+      });
     };
 
     $scope.forkScenario = function() {
@@ -40,18 +47,18 @@ define(['angular', 'underscore', 'mcda/config'], function(angular, _, Config) {
     $scope.isEditTitleVisible = false;
     $scope.scenarioTitle = {};
 
-    $scope.editTitle = function () {
+    $scope.editTitle = function() {
       $scope.isEditTitleVisible = true;
       $scope.scenarioTitle.value = $scope.scenario.title;
     };
 
-    $scope.saveTitle = function () {
+    $scope.saveTitle = function() {
       $scope.scenario.title = $scope.scenarioTitle.value;
       $scope.scenario.save();
       $scope.isEditTitleVisible = false;
     };
 
-    $scope.cancelTitle = function () {
+    $scope.cancelTitle = function() {
       $scope.isEditTitleVisible = false;
     };
   };
