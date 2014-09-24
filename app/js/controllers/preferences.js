586eb54f35ff674c91306345de1f89527c46b870
Daan Reid <osmosisch@gmail.com>
2014-09-24 16:19:28 +0200

    Cleanup preferences controller

--- app/js/controllers/preferences.js
+++ app/js/controllers/preferences.js
@@ -3,129 +3,79 @@ define(['mcda/config', 'mcda/lib/patavi', 'angular', 'angularanimate', 'mmfounda
   function(Config, patavi, angular, angularanimate, mmfoundation, _) {
     var dependencies = ['$scope', '$location', '$anchorScroll', 'PartialValueFunction', 'Tasks', 'TaskDependencies', 'intervalHull', 'taskDefinition'];
     var PreferencesController = function($scope, $location, $anchorScroll, PartialValueFunction, Tasks, TaskDependencies, intervalHull, taskDefinition) {
-      var state;
+      var state = taskDefinition.clean($scope.scenario.state);
 
       $scope.$parent.taskId = taskDefinition.id;
       $scope.intervalHull = intervalHull;
-      $scope.scenario.$promise.then(function(scenario) {
-        state = taskDefinition.clean(scenario.state);
-        $scope.criteria = _.sortBy(_.map(_.pairs($scope.scenario.state.problem.criteria), function(crit, idx) {
-          return _.extend(crit[1], {
-            id: crit[0],
-            w: 'w_' + (idx + 1)
-          });
-        }), 'w');
-        var eqns = _.map(state.prefs, function(pref) {
-          var crit = _.map(pref.criteria, w);
-          if (pref.type === 'ordinal') {
-            return crit[0] + ' & \\geq & ' + crit[1] + '\\\\';
-          } else if (pref.type === 'ratio bound') {
-            return '\\frac{' + crit[0] + '}{' + crit[1] + '} & \\in & [' + pref.bounds[0].toFixed(3) + ', ' + pref.bounds[1].toFixed(3) + '] \\\\';
-          } else if (pref.type === 'exact swing') {
-            return '\\frac{' + crit[0] + '}{' + crit[1] + '} & = & ' + pref.ratio.toFixed(3) + ' \\\\';
-          } else {
-            console.error('Unsupported preference type ', pref);
-            return '';
-          }
+
+      var w = function(criterionKey) {
+        return _.find($scope.criteria, function(crit) {
+          return crit.id === criterionKey;
+        }).w;
+      };
+
+      $scope.criteria = _.sortBy(_.map(_.pairs($scope.scenario.state.problem.criteria), function(crit, idx) {
+        return _.extend(crit[1], {
+          id: crit[0],
+          w: 'w_' + (idx + 1)
         });
-        var eqnArray = '\\begin{eqnarray} ' + _.reduce(eqns, function(memo, eqn) {
-          return memo + eqn;
-        }, '') + ' \\end{eqnarray}';
-        $scope.preferences = eqnArray;
+      }), 'w');
 
-        // FIXME: these calculations really should happen at the workspace level
-        // ===========================================
-        (function(problem) {
-          var errorHandler = function(code, error) {
-            var message = {
-              code: (code && code.desc) ? code.desc : code,
-              cause: error
-            };
-            $scope.$root.$broadcast('error', message);
-          };
-          var data = _.extend(problem, {
-            'method': 'scales'
-          });
-          var task = patavi.submit(Config.pataviService, data);
-          $scope.scales = {};
-          task.results.then(function(data) {
-            $scope.$apply(function() {
-              $scope.scales = data.results;
-            });
-          }, errorHandler);
-        })($scope.workspace.problem);
-        // ===========================================
+      var eqns = _.map(state.prefs, function(pref) {
+        var crit = _.map(pref.criteria, w);
+        if (pref.type === 'ordinal') {
+          return crit[0] + ' & \\geq & ' + crit[1] + '\\\\';
+        } else if (pref.type === 'ratio bound') {
+          return '\\frac{' + crit[0] + '}{' + crit[1] + '} & \\in & [' + pref.bounds[0].toFixed(3) + ', ' + pref.bounds[1].toFixed(3) + '] \\\\';
+        } else if (pref.type === 'exact swing') {
+          return '\\frac{' + crit[0] + '}{' + crit[1] + '} & = & ' + pref.ratio.toFixed(3) + ' \\\\';
+        } else {
+          console.error('Unsupported preference type ', pref);
+          return '';
+        }
+      });
+      var eqnArray = '\\begin{eqnarray} ' + _.reduce(eqns, function(memo, eqn) {
+        return memo + eqn;
+      }, '') + ' \\end{eqnarray}';
+      $scope.preferences = eqnArray;
 
+      $scope.isAccessible = function(task, state) {
+        return TaskDependencies.isAccessible(task, state);
+      };
 
-        $scope.isAccessible = function(task, state) {
-          return TaskDependencies.isAccessible(task, state);
-        };
-
-        $scope.isScaleRangePresent = function() {
-          var isPresent = _.every($scope.scenario.state.problem.criteria, function(criterion) {
-            return criterion.pvf && criterion.pvf.range;
-          });
-          return isPresent;
-        };
-
-        $scope.isPartialValueFunctionPresent = function() {
-          return _.every($scope.scenario.state.problem.criteria, function(criterion) {
-            var pvf = criterion.pvf;
-            return pvf && pvf.direction && pvf.type;
-          });
-        };
-
-        $scope.isOrdinalSwingPresent = function() {
-          return $scope.scenario.state.prefs;
-        };
-
-        $scope.isExactSwingPresent = function() {
-          return $scope.scenario.state.prefs && $scope.scenario.state.prefs[1] && $scope.scenario.state.prefs[1].type === 'exact swing';
-        };
-
-        $scope.isIntervalSwingPresent = function() {
-          return $scope.scenario.state.prefs && $scope.scenario.state.prefs[1] && $scope.scenario.state.prefs[1].type === 'ratio bound';
-        };
-
-        $scope.dependenciesString = function(dependencies) {
-          var result = '';
-          _.each(dependencies, function(dep) {
-            result = result + '\n- ' + TaskDependencies.definitions[dep].title;
-          });
-          return result;
-        };
-
-        $scope.scrollToPVFs = function() {
-          $location.hash('partial-value-functions');
-          $anchorScroll();
-        };
-
-        // var linearPVFs = _.filter($scope.scenario.state.problem.criteria, function(criterion) {
-        //   return criterion.pvf && criterion.pvf.type === 'linear';
-        // }).length;
-        // var pvfStatus = 'linear';
-
-        // if (linearPVFs === 0) {
-        //   pvfStatus = 'piece-wise linear';
-        // } else if (linearPVFs < _.size($scope.scenario.state.problem.criteria)) {
-        //   pvfStatus = linearPVFs + ' linear; ' + (_.size($scope.scenario.state.problem.criteria) - linearPVFs) + ' piece-wise linear';
-        // }
-
-        $scope.problem = $scope.workspace.problem;
-
-
-        var w = function(criterionKey) {
-          return _.find($scope.criteria, function(crit) {
-            return crit.id === criterionKey;
-          }).w;
-        };
-
-
-
-        $scope.getXY = _.memoize(PartialValueFunction.getXY, function(arg) {
-          return angular.toJson(arg.pvf);
+      $scope.isScaleRangePresent = function() {
+        var isPresent = _.every($scope.scenario.state.problem.criteria, function(criterion) {
+          return criterion.pvf && criterion.pvf.range;
         });
+        return isPresent;
+      };
 
+      $scope.isPartialValueFunctionPresent = function() {
+        return _.every($scope.scenario.state.problem.criteria, function(criterion) {
+          var pvf = criterion.pvf;
+          return pvf && pvf.direction && pvf.type;
+        });
+      };
+
+      $scope.isOrdinalSwingPresent = function() {
+        return $scope.scenario.state.prefs;
+      };
+
+      $scope.isExactSwingPresent = function() {
+        return $scope.scenario.state.prefs && $scope.scenario.state.prefs[1] && $scope.scenario.state.prefs[1].type === 'exact swing';
+      };
+
+      $scope.isIntervalSwingPresent = function() {
+        return $scope.scenario.state.prefs && $scope.scenario.state.prefs[1] && $scope.scenario.state.prefs[1].type === 'ratio bound';
+      };
+
+      $scope.scrollToPVFs = function() {
+        $location.hash('partial-value-functions');
+        $anchorScroll();
+      };
+
+      $scope.getXY = _.memoize(PartialValueFunction.getXY, function(arg) {
+        return angular.toJson(arg.pvf);
       });
 
     };
