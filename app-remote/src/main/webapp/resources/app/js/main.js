055fb7bad5d0d025898d5501ae609279f679b73c
Joel Kuiper <me@joelkuiper.eu>
2015-02-09 14:34:59 +0100

    Do not defer rendering of Angular
    no longer necessary

--- app-remote/src/main/webapp/resources/app/js/main.js
+++ app-remote/src/main/webapp/resources/app/js/main.js
@@ -31,10 +31,7 @@ require.config({
   priority: ['angular']
 });
 
-window.name = "NG_DEFER_BOOTSTRAP!";
 
 require(['require', 'angular', 'mcda/mcda-web'], function (require, angular) {
-  require(['domReady!'], function (document) {
-    angular.bootstrap(document , ['elicit']);
-  });
+  angular.bootstrap(document , ['elicit']);
 });
