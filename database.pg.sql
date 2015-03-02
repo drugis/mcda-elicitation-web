29822365f6cf574821475dba0dbc6e2187a6ab91
bobgoe <bobgoeree@gmail.com>
2015-03-02 10:16:34 +0100

    Use changeset for json columns
    changeset 6 instead of altering changeset 4 & 5

--- database.pg.sql
+++ database.pg.sql
@@ -28,7 +28,7 @@ create table Account (id SERIAL NOT NULL,
 create table Workspace (id SERIAL NOT NULL,
             owner int,
             title varchar not null,
-            problem json not null,
+            problem TEXT not null,
             defaultScenarioId int,
             primary key (id),
             FOREIGN KEY(owner) REFERENCES Account(id));
@@ -36,7 +36,7 @@ create table Workspace (id SERIAL NOT NULL,
 create table Scenario (id SERIAL NOT NULL,
             workspace int,
             title varchar not null,
-            state json not null,
+            state TEXT not null,
             primary key (id),
             FOREIGN KEY(workspace) REFERENCES Workspace(id));
 
@@ -53,8 +53,15 @@ CREATE TABLE AccountRoles (
 );
 
 -- changeset reidd:5
+
 CREATE TABLE Remarks (
   workspaceId INT NOT NULL,
-  remarks json NOT NULL,
+  remarks VARCHAR NOT NULL,
   PRIMARY KEY (workspaceId),
   FOREIGN KEY(workspaceId) REFERENCES Workspace(id));
+	
+-- changeset bobgoe:6
+
+ALTER TABLE Workspace ALTER COLUMN problem TYPE JSON USING problem::JSON;
+ALTER TABLE Scenario ALTER COLUMN state TYPE JSON USING state::JSON;
+ALTER TABLE Remarks ALTER COLUMN remarks TYPE JSON USING remarks::JSON;
