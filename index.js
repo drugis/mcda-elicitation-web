721de44e5796a0e3da0832f3616358278f39b297
Connor Stroomberg <c.stroomberg@gmail.com>
2016-01-08 15:50:56 +0100

    fix get remarks

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var loginUtils = require('./node-backend/loginUtils');
 var logger = require('./node-backend/logger');
 
 var express = require('express'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    csurf = require('csurf');
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  csurf = require('csurf');
 
 var pg = require('pg');
 var deferred = require('deferred');
@@ -30,7 +30,7 @@ everyauth.google
   .appId(conf.google.clientId)
   .appSecret(conf.google.clientSecret)
   .scope('https://www.googleapis.com/auth/userinfo.profile email')
-  .findOrCreateUser(function (sess, accessToken, extra, googleUser) {
+  .findOrCreateUser(function(sess, accessToken, extra, googleUser) {
     var user = this.Promise();
     pg.connect(conf.pgConStr, function(error, client, done) {
       if (error) return console.error("Error fetching client from pool", error);
@@ -41,18 +41,16 @@ everyauth.google
         }
         if (result.rows.length == 0) {
           client.query("INSERT INTO UserConnection (userId, providerId, providerUserId, rank, displayName, profileUrl, accessToken, refreshToken, expireTime)" +
-            " VALUES ($1, 'google', $2, 1, $3, $4, $5, $6, $7)",
-            [googleUser.id, googleUser.id, googleUser.name, googleUser.link, accessToken, extra.refresh_token, extra.expires_in],
+            " VALUES ($1, 'google', $2, 1, $3, $4, $5, $6, $7)", [googleUser.id, googleUser.id, googleUser.name, googleUser.link, accessToken, extra.refresh_token, extra.expires_in],
             function(error, result) {
               if (error) {
                 done();
                 return user.fail(error);
               }
-              client.query("INSERT INTO Account (username, firstName, lastName) VALUES ($1, $2, $3) RETURNING id",
-                [googleUser.id, googleUser.given_name, googleUser.family_name],
+              client.query("INSERT INTO Account (username, firstName, lastName) VALUES ($1, $2, $3) RETURNING id", [googleUser.id, googleUser.given_name, googleUser.family_name],
                 function(error, result) {
                   done();
-                  if (error)  {
+                  if (error) {
                     return user.fail(error);
                   }
                   var row = result.rows[0];
@@ -60,7 +58,8 @@ everyauth.google
                     "id": row.id,
                     "username": googleUser.id,
                     "firstName": googleUser.given_name,
-                    "lastName": googleUser.family_name});
+                    "lastName": googleUser.family_name
+                  });
                 });
             });
           return;
@@ -76,10 +75,7 @@ everyauth.google
 
 var bower_path = '/bower_components';
 var csrfValue = function(req) {
-  var token = (req.body && req.body._csrf)
-    || (req.query && req.query._csrf)
-    || (req.headers['x-csrf-token'])
-    || (req.headers['x-xsrf-token']);
+  var token = (req.body && req.body._csrf) || (req.query && req.query._csrf) || (req.headers['x-csrf-token']) || (req.headers['x-xsrf-token']);
   return token;
 };
 var app = express();
@@ -92,20 +88,25 @@ app
   .use(cookieParser('very secret secret'))
   .use(session())
   .use(everyauth.middleware(app))
-  .use(csurf({ cookie: true }));
+  .use(csurf({
+    cookie: true
+  }));
 
 function requireUserIsOwner(req, res, next) {
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error fetching client from pool', err);
     }
     client.query('SELECT owner FROM workspace WHERE id = $1', [req.params.id], function(err, result) {
       done();
-      if(err) {
+      if (err) {
         return console.error('error running query', err);
       }
       if (!req.user || result.rows[0].owner != req.user.id) {
-        res.status(403).send({"code":403,"message":"Access to resource not authorised"});
+        res.status(403).send({
+          "code": 403,
+          "message": "Access to resource not authorised"
+        });
       } else {
         next();
       }
@@ -118,7 +119,7 @@ router.get('/workspaces/:id*', requireUserIsOwner);
 router.post('/workspaces/:id*', requireUserIsOwner);
 app.use(router);
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   if (req.user) {
     res.cookie('LOGGED-IN-USER', JSON.stringify(req.user));
@@ -145,12 +146,12 @@ app.get("/index", function(req, res, next) {
 app.get("/workspaces", function(req, res) {
   logger.debug('GET /workspaces');
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error fetching client from pool', err);
     }
     client.query('SELECT id, owner, title, problem, defaultScenarioId AS "defaultScenarioId" FROM Workspace WHERE owner = $1', [req.user.id], function(err, result) {
       done();
-      if(err) {
+      if (err) {
         return console.error('error running query', err);
       }
       res.send(result.rows);
@@ -161,33 +162,35 @@ app.get("/workspaces", function(req, res) {
 app.post("/workspaces", function(req, res) {
   logger.debug('POST /workspaces');
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error entering workspace in pool', err);
     }
     client.query('INSERT INTO workspace (owner, title, problem) VALUES ($1, $2, $3) RETURNING id', [req.user.id, req.body.title, req.body.problem], function(err, result) {
-      if(err) {
+      if (err) {
         done();
         return console.error('error running query', err);
       }
       var workspaceId = result.rows[0].id;
       client.query('INSERT INTO remarks (workspaceid, remarks) VALUES ($1, $2)', [workspaceId, {}], function(err, result) {
-        if(err) {  
-        done();
+        if (err) {
+          done();
           return console.error('error running query', err);
         }
-        client.query('INSERT INTO scenario (workspace, title, state) VALUES ($1, $2, $3) RETURNING id', [workspaceId, 'Default', { problem: req.body.problem }], function(err, result) {
-          if(err) {  
-          done();
+        client.query('INSERT INTO scenario (workspace, title, state) VALUES ($1, $2, $3) RETURNING id', [workspaceId, 'Default', {
+          problem: req.body.problem
+        }], function(err, result) {
+          if (err) {
+            done();
             return console.error('error running query', err);
           }
           var scenarioId = result.rows[0].id;
           client.query('UPDATE workspace SET defaultScenarioId = $1 WHERE id = $2', [scenarioId, workspaceId], function(err, result) {
-            if(err) {
+            if (err) {
               done();
               return console.error('error running query', err);
             }
             client.query('SELECT id, owner, problem, defaultScenarioId AS "defaultScenarioId" FROM workspace WHERE id = $1', [workspaceId], function(err, result) {
-              if(err) {
+              if (err) {
                 done();
                 return console.error('error running query', err);
               }
@@ -204,19 +207,16 @@ app.post("/workspaces", function(req, res) {
 app.get("/workspaces/:id", function(req, res) {
   logger.debug('GET /workspaces/:id');
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error fetching workspace from pool', err);
     }
-    client.query('SELECT id, owner, problem, defaultScenarioId AS "defaultScenarioId" FROM workspace WHERE id = $1', [req.params.id], function(err, result) {
+    client.query('SELECT id, owner, problem::jsonb, defaultScenarioId AS "defaultScenarioId" FROM workspace WHERE id = $1', [req.params.id], function(err, result) {
       done();
-      if(err) {
+      if (err) {
         return console.error('error running query', err);
       }
 
-      // bug fix for mcda-node-version, maybe change table structure to result json instead of string 'state'
-      var workspace = result.rows[0];
-      workspace.problem = JSON.parse(workspace.problem);
-      res.send(workspace);
+      res.send(result.rows[0]);
     });
   });
 });
@@ -224,12 +224,12 @@ app.get("/workspaces/:id", function(req, res) {
 app.get("/workspaces/:id/scenarios", function(req, res) {
   logger.debug('GET /workspaces/:id/scenarios');
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error fetching workspace from pool', err);
     }
     client.query('SELECT id, title, state, workspace AS "workspaceId" FROM scenario WHERE workspace = $1', [req.params.id], function(err, result) {
       done();
-      if(err) {
+      if (err) {
         return console.error('error running query', err);
       }
       res.send(result.rows);
@@ -238,35 +238,36 @@ app.get("/workspaces/:id/scenarios", function(req, res) {
 });
 
 app.get("/workspaces/:id/scenarios/:id", function(req, res) {
-    logger.debug('GET /workspaces/:id/scenarios/:id');
+  logger.debug('GET /workspaces/:id/scenarios/:id');
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error fetching workspace from pool', err);
     }
-    client.query('SELECT id, title, state, workspace AS "workspaceId" FROM scenario WHERE id = $1', [req.params.id], function(err, result) {
+    client.query('SELECT id, title, state::jsonb, workspace AS "workspaceId" FROM scenario WHERE id = $1', [req.params.id], function(err, result) {
       done();
-      if(err) {
+      if (err) {
         return console.error('error running query', err);
       }
 
-      // bug fix for mcda-node-version, maybe change table structure to result json instead of string 'state'
-      var scenario = result.rows[0];
-      scenario.state = JSON.parse(scenario.state);
-      
-      res.send(scenario);
+      res.send(result.rows[0]);
     });
   });
 });
 
 app.get("/workspaces/:id/remarks", function(req, res) {
+  logger.debug('GET /workspaces/:id/remarks');
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
-      return console.error('error fetching remarks from pool', err);
+    if (err) {
+      var message = 'error fetching remarks from pool';
+      logger.error(message, err);
+      res.status(500).send(message);
     }
-    client.query('SELECT workspaceid AS "workspaceId", remarks FROM remarks WHERE workspaceid = $1', [req.params.id], function(err, result) {
+    client.query('SELECT workspaceid AS "workspaceId", remarks::jsonb FROM remarks WHERE workspaceid = $1', [req.params.id], function(err, result) {
       done();
-      if(err) {
-        return console.error('error running query', err);
+      if (err) {
+        var message = 'error fetching remarks from database';
+        logger.error(message, err);
+        res.status(500).send(message);
       }
       res.send(result.rows[0]);
     });
@@ -275,12 +276,12 @@ app.get("/workspaces/:id/remarks", function(req, res) {
 
 app.post("/workspaces/:id/remarks", function(req, res) {
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error fetching remarks from pool', err);
     }
     client.query('UPDATE remarks SET remarks = $1 WHERE workspaceid = $2', [req.body.remarks, req.params.id], function(err, result) {
       done();
-      if(err) {
+      if (err) {
         return console.error('error running query', err);
       }
       res.send(result.rows[0]);
@@ -290,12 +291,12 @@ app.post("/workspaces/:id/remarks", function(req, res) {
 
 app.post("/workspaces/:id", function(req, res) {
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error fetching remarks from pool', err);
     }
     client.query('UPDATE workspace SET title = $1, problem = $2 WHERE id = $3 ', [req.body.problem.title, req.body.problem, req.params.id], function(err, result) {
       done();
-      if(err) {
+      if (err) {
         return console.error('error running query', err);
       }
       res.send(result.rows[0]);
@@ -305,12 +306,14 @@ app.post("/workspaces/:id", function(req, res) {
 
 app.post("/workspaces/:id/scenarios", function(req, res) {
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error fetching remarks from pool', err);
     }
-    client.query('INSERT INTO scenario (workspace, title, state) VALUES ($1, $2, $3) RETURNING id', [req.params.id, req.body.title, { problem: req.body.state.problem }], function(err, result) {
+    client.query('INSERT INTO scenario (workspace, title, state) VALUES ($1, $2, $3) RETURNING id', [req.params.id, req.body.title, {
+      problem: req.body.state.problem
+    }], function(err, result) {
       done();
-      if(err) {
+      if (err) {
         return console.error('error running query', err);
       }
       res.send(result.rows[0]);
@@ -320,12 +323,14 @@ app.post("/workspaces/:id/scenarios", function(req, res) {
 
 app.post("/workspaces/:id/scenarios/:id", function(req, res) {
   pg.connect(conf.pgConStr, function(err, client, done) {
-    if(err) {
+    if (err) {
       return console.error('error fetching remarks from pool', err);
     }
-    client.query('UPDATE scenario SET state = $1, title = $2 WHERE id = $3', [{ problem: req.body.state.problem }, req.body.title, req.body.id], function(err, result) {
+    client.query('UPDATE scenario SET state = $1, title = $2 WHERE id = $3', [{
+      problem: req.body.state.problem
+    }, req.body.title, req.body.id], function(err, result) {
       done();
-      if(err) {
+      if (err) {
         return console.error('error running query', err);
       }
       res.send(result.rows[0]);
@@ -336,12 +341,12 @@ app.post("/workspaces/:id/scenarios/:id", function(req, res) {
 app.get('/user', loginUtils.emailHashMiddleware);
 
 //FIXME: should not be needed?
-app.get("/main.js", function(req, res, next) { 
+app.get("/main.js", function(req, res, next) {
   res.sendfile(__dirname + '/app/js/main.js');
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
-app.get('*', function(req, res){
+app.get('*', function(req, res) {
   res.status(404).sendfile(__dirname + '/public/error.html');
 });
 
