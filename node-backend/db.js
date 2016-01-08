870ee419f7f9b17e33ddf20cb21262cbc908dd4c
Connor Stroomberg <c.stroomberg@gmail.com>
2016-01-08 16:57:53 +0100

    add db helper

---
+++ node-backend/db.js
@@ -0,0 +1,24 @@
+'use strict';
+var pg = require('pg');
+var logger = require('./logger');
+
+module.exports = function(url) {
+  var dbUrl = url;
+  logger.debug('db connection: ' + url)
+  return {
+    query: function(text, values, callback) {
+      logger.debug('db.query; text: ' + text + ' values' + values);
+      pg.connect(dbUrl, function(err, client, done) {
+        if (err) {
+          logger.error(err);
+          callback(err);
+          return done();
+        }
+        var query = client.query(text, values, function(err, result) {
+          done();
+          callback(err, result);
+        });
+      });
+    }
+  };
+}
