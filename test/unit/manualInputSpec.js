4a7fa5371985f08301ab89813d03b6342ab54ddb
Joris de Keijser <chinook25@gmail.com>
2017-05-02 10:50:59 +0200

    layout fixed, needs tests

--- test/unit/manualInputSpec.js
+++ test/unit/manualInputSpec.js
@@ -3,9 +3,14 @@ define(['angular',
     'angular-mocks',
     'mcda/manualInput/manualInputService'
   ],
-  function() {
-    describe('The manualInputService', function() {
-      beforeEach(module('manualInput.manualInputService'));
+  function(angular,angularMocks, ManualInputService) {
+    fdescribe('The manualInputService', function() {
+      var manualInputService;
+
+      beforeEach(function() {
+        module('manualInput.manualInputService');
+          manualInputService = ManualInputService;
+      });
 
       describe('createProblem', function() {
         it('should create a problem, ready to go to the workspace', function() {
@@ -19,8 +24,28 @@ define(['angular',
               name: 'treatment2'
             }
           };
-          var criteria = {};
-          var result = ManualInputService.createProblem(criteria, treatments, title, description, performanceTable);
+          var criteria = {
+            title: 'criterion title',
+            description: 'some crit description',
+            unitOfMeasurement: 'particles',
+            scale: [0, 1]
+          };
+          var performanceTable = [{
+            alternative: 'treatment1',
+            criterion: 'criterion title',
+            performance: {
+              type: 'exact',
+              value: 10
+            }
+          }, {
+            alternative: 'treatment2',
+            criterion: 'criterion title',
+            performance: {
+              type: 'exact',
+              value: 5
+            }
+          }];
+          var result = manualInputService.createProblem(criteria, treatments, title, description, performanceTable);
           var expectedResult = {};
           expect(result).toEqual(expectedResult);
         });
