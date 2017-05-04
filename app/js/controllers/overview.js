e1996ceb7e344f2c466199410ec44e727498cf5a
Joris de Keijser <chinook25@gmail.com>
2017-05-04 16:10:54 +0200

    redesign overview tab mcda

--- app/js/controllers/overview.js
+++ app/js/controllers/overview.js
@@ -1,8 +1,57 @@
 'use strict';
 define(function() {
-  return function($scope, taskDefinition, ValueTreeUtil) {
-    $scope.problem = $scope.workspace.problem;
-    $scope.expandedValueTree = ValueTreeUtil.addCriteriaToValueTree($scope.workspace.$$valueTree, $scope.problem.criteria);
-  };
+  var _ = require('lodash');
+  return function($scope, $stateParams, RemarksResource, EffectsTableResource, EffectsTableService) {
+   var remarksCache;
+    $scope.scales = $scope.workspace.$$scales.observed;
+    $scope.valueTree = $scope.workspace.$$valueTree;
 
+    $scope.problem = $scope.workspace.problem;
+    $scope.effectsTableData = EffectsTableService.buildEffectsTableData($scope.problem, $scope.valueTree);
+    $scope.nrAlternatives = _.keys($scope.problem.alternatives).length;
+    $scope.remarks = {};
+    $scope.alternativeInclusions = {};
+
+    _.map($scope.problem.alternatives, function(alternative, alternativeKey){
+      $scope.alternativeInclusions[alternativeKey] = true;
+    });
+    
+    EffectsTableResource.query($stateParams, function(exclusions) {
+      _.forEach(exclusions, function(exclusion) {
+        $scope.alternativeInclusions[exclusion.alternativeId] = false;
+      });
+    });
+
+    RemarksResource.get(_.omit($stateParams, 'id'), function(remarks) {
+      if (remarks.remarks) {
+        $scope.remarks = remarks;
+      remarksCache = _.cloneDeep(remarks);
+      }
+    });
+
+    $scope.$watch('workspace.$$scales.observed', function(newValue) {
+      $scope.scales = newValue;
+    }, true);
+    
+    $scope.isExact = function(criterion, alternative) {
+      var perf = _.find($scope.problem.performanceTable, function(performance) {
+        return performance.alternative === alternative && performance.criterion === criterion;
+      });
+      return !!perf && perf.performance.type === 'exact';
+    };
+
+    $scope.toggleVisibility = function(alternativeId){
+      EffectsTableResource.toggleExclusion($stateParams, {alternativeId: alternativeId});
+    };
+
+    $scope.saveRemarks = function() {
+        remarksCache = _.cloneDeep($scope.remarks);
+      RemarksResource.save(_.omit($stateParams, 'id'), $scope.remarks, function() {
+      });
+    };
+
+      $scope.remarks = _.cloneDeep(remarksCache);
+    $scope.cancelRemarks = function() {
+    };
+  };
 });
