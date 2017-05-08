c50888760a277af0f8d9ef582f915e9acf9227fa
Joris de Keijser <chinook25@gmail.com>
2017-05-08 09:41:49 +0200

    don't discard criterion titles

--- node-backend/util.js
+++ node-backend/util.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 
 function reduceProblem(problem) {
   var criteria = _.reduce(problem.criteria, function(accum, criterion, key) {
-    accum[key] = _.pick(criterion, ['scale', 'pvf']);
+    accum[key] = _.pick(criterion, ['scale', 'pvf', 'title']);
     return accum;
   }, {});
   return {
