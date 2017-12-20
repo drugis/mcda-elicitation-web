00f56a83a58be49805fbad851777a6d3079c6035
Daan Reid <osmosisch@gmail.com>
2017-12-20 11:46:49 +0100

    Upload problem validation

--- app/js/services/workspaceService.js
+++ app/js/services/workspaceService.js
@@ -172,6 +172,278 @@ define(['lodash', 'angular'], function(_) {
       });
     }
 
+    /*
+     * workspace should have:
+     * - title
+     * - criteria
+     * - alternatives
+     * - performanceTable
+     * optional:
+     * - description
+     * - preferences
+     */
+    function validateWorkspace(workspace) {
+      var constraints = [missingProperties,
+        tooFewCriteria,
+        tooFewAlternatives,
+        criterionLackingTitle,
+        alternativeLackingTitle,
+        performanceTableWithInvalidAlternative,
+        performanceTableWithInvalidCriterion,
+        performanceTableWithMissingData,
+        preferencesWithUnfoundCriterion,
+        mixedPreferenceTypes,
+        inconsistentOrdinalPreferences,
+        inconsistentExactPreferences,
+        relativePerformanceLackingBaseline,
+        relativePerformanceWithBadMu,
+        relativePerformanceWithBadCov
+
+      ];
+      var triggeredConstraint;
+      try {
+        triggeredConstraint = _.find(constraints, function(constraint) {
+          return constraint(workspace);
+        });
+      } catch (exception) {
+        return {
+          isValid: false,
+          errorMessage: 'Exception while reading problem. Please make sure the file follows the specifications as laid out in the manual'
+        };
+      }
+      return {
+        isValid: !triggeredConstraint,
+        errorMessage: triggeredConstraint ? triggeredConstraint(workspace) : undefined
+      };
+    }
+
+    // privates
+    function missingProperties(workspace) { // required properties
+      var requiredProperties = ['title',
+        'criteria',
+        'alternatives',
+        'performanceTable'
+      ];
+
+      var missingProperties = _.reject(requiredProperties, function(property) {
+        return workspace[property];
+      });
+
+      if (missingProperties.length === 1) {
+        return 'Missing workspace property: ' + missingProperties[0];
+      } else if (missingProperties.length > 1) {
+        return 'Missing workspace properties: ' + missingProperties.join(', ');
+      }
+    }
+
+    function tooFewCriteria(workspace) {
+      if (_.size(workspace.criteria) < 2) {
+        return 'Two or more criteria required';
+      }
+    }
+
+    function tooFewAlternatives(workspace) {
+      if (_.size(workspace.alternatives) < 2) {
+        return 'Two or more alternatives required';
+      }
+    }
+
+    function performanceTableWithInvalidAlternative(workspace) {
+      var entry = _.find(workspace.performanceTable, function(tableEntry) {
+        return isAbsolutePerformance(tableEntry) && !workspace.alternatives[tableEntry.alternative];
+      });
+      if (entry) {
+        return 'Performance table contains data for nonexistent alternative: "' + entry.alternative + '"';
+      }
+    }
+
+    function performanceTableWithInvalidCriterion(workspace) {
+      var entry = _.find(workspace.performanceTable, function(tableEntry) {
+        return !workspace.criteria[tableEntry.criterion];
+      });
+      if (entry) {
+        return 'Performance table contains data for nonexistent criterion: "' + entry.criterion + '"';
+      }
+    }
+
+    function performanceTableWithMissingData(workspace) {
+      var alternatives = _.keys(workspace.alternatives);
+      var criteria = _.keys(workspace.criteria);
+      var criteriaWithAbsolutePerformance = _.filter(criteria, function(criterion) {
+        return _.find(workspace.performanceTable, function(tableEntry) {
+          return tableEntry.criterion === criterion && isAbsolutePerformance(tableEntry);
+        });
+      });
+      var critAltCombinations = _.reduce(criteriaWithAbsolutePerformance, function(acc, criterion) {
+        return acc.concat(_.map(alternatives, function(alternative) {
+          return {
+            criterion: criterion,
+            alternative: alternative
+          };
+        }));
+      }, []);
+      var missingPair = _.find(critAltCombinations, function(critAltCombo) {
+        return !_.find(workspace.performanceTable, function(tableEntry) {
+          return tableEntry.alternative === critAltCombo.alternative && tableEntry.criterion === critAltCombo.criterion;
+        });
+      });
+      if (missingPair) {
+        return 'Performance table is missing data for criterion "' + missingPair.criterion +
+          '" and alternative "' + missingPair.alternative + '"';
+      }
+    }
+
+    function preferencesWithUnfoundCriterion(workspace) {
+      var crit;
+      _.find(workspace.preferences, function(preference) {
+        return _.find(preference.criteria, function(criterion) {
+          if (!workspace.criteria[criterion]) {
+            crit = criterion;
+            return true;
+          } else {
+            return false;
+          }
+        });
+      });
+      if (crit) {
+        return 'Preferences contain data for nonexistent criterion: "' + crit + '"';
+      }
+    }
+
+    function mixedPreferenceTypes(workspace) {
+      if (hasNoPreferences(workspace)) {
+        return;
+      }
+      var firstType = workspace.preferences[0].type;
+      if (_.find(workspace.preferences, function(preference) {
+          return preference.type !== firstType;
+        })) {
+        return 'Preferences should all be the same type';
+      }
+    }
+
+    function inconsistentOrdinalPreferences(workspace) {
+      if (!workspace.preferences || _.isEmpty(workspace.preferences) || _.find(workspace.preferences, function(preference) {
+          return preference.type !== 'ordinal';
+        })) {
+        return;
+      }
+      if (workspace.preferences.length !== _.size(workspace.criteria) - 1) {
+        return 'Inconsistent ordinal preferences';
+      }
+
+      var visitCount = _.cloneDeep(workspace.criteria);
+      _.forEach(visitCount, function(node) {
+        node.visits = 0;
+      });
+
+      // always visit beginning of path
+      ++visitCount[workspace.preferences[0].criteria[0]].visits;
+
+      var badPath = false;
+      _.forEach(workspace.preferences, function(preference, index) {
+        var origin = preference.criteria[0];
+        var destination = preference.criteria[1];
+        ++visitCount[destination].visits;
+        if (index > 0) {
+          var previousDestination = workspace.preferences[index - 1].criteria[1];
+          badPath = origin !== previousDestination;
+        }
+      });
+
+      var badCoverage = _.find(visitCount, ['visits', 0]) || _.find(visitCount, function(node) {
+        return node.visits > 1;
+      });
+
+      if (badPath || badCoverage) {
+        return 'Inconsistent ordinal preferences';
+      }
+    }
+
+    function inconsistentExactPreferences(workspace) {
+      if (hasNoPreferences(workspace) || workspace.preferences[0].type !== 'exact swing') {
+        return;
+      }
+
+      var first = workspace.preferences[0].criteria[0];
+      if (_.find(workspace.preferences, function(preference) {
+          return preference.criteria[0] !== first ||
+            preference.criteria[0] === preference.criteria[1] ||
+            preference.ratio > 1 ||
+            preference.ratio <= 0;
+        })) {
+        return 'Inconsistent exact weighting preferences';
+      }
+    }
+
+    function hasNoPreferences(workspace) {
+      return !workspace.preferences || _.isEmpty(workspace.preferences);
+    }
+
+    function isAbsolutePerformance(tableEntry) {
+      return !tableEntry.performance.type.startsWith('relative');
+    }
+
+    function criterionLackingTitle(workspace) {
+      var key;
+      var crit = _.find(workspace.criteria, function(criterion, critKey) {
+        key = critKey;
+        return !criterion.title;
+      });
+      if (crit) {
+        return 'Missing title for criterion: "' + key + '"';
+      }
+    }
+
+    function alternativeLackingTitle(workspace) {
+      var key;
+      var alt = _.find(workspace.alternatives, function(alternative, critKey) {
+        key = critKey;
+        return !alternative.title;
+      });
+      if (alt) {
+        return 'Missing title for alternative: "' + key + '"';
+      }
+    }
+
+    function relativePerformanceLackingBaseline(workspace) {
+      var entryLackingBaseline = _.find(workspace.performanceTable, function(tableEntry) {
+        return !isAbsolutePerformance(tableEntry) && !tableEntry.performance.parameters.baseline;
+      });
+      if (entryLackingBaseline) {
+        return 'Missing baseline for criterion: "' + entryLackingBaseline.criterion + '"';
+      }
+    }
+
+    function relativePerformanceWithBadMu(workspace) {
+      var alternativeKey;
+      var entryWithBadMu = _.find(workspace.performanceTable, function(tableEntry) {
+        return !isAbsolutePerformance(tableEntry) && _.find(tableEntry.performance.parameters.relative.mu, function(muValue, muKey) {
+          alternativeKey = muKey;
+          return !workspace.alternatives[muKey];
+        });
+      });
+      if (entryWithBadMu) {
+        return 'The mu of the performance of criterion: "' + entryWithBadMu.criterion + '" refers to nonexistent alternative: "' + alternativeKey + '"';
+      }
+    }
+
+    function relativePerformanceWithBadCov(workspace) {
+      var alternativeKey;
+      var entryWithBadCov = _.find(workspace.performanceTable, function(tableEntry) {
+        return !isAbsolutePerformance(tableEntry) && (_.find(tableEntry.performance.parameters.relative.cov.rownames, function(rowVal) {
+          alternativeKey = rowVal;
+          return !workspace.alternatives[rowVal];
+        }) || _.find(tableEntry.performance.parameters.relative.cov.colnames, function(colVal) {
+          alternativeKey = colVal;
+          return !workspace.alternatives[colVal];
+        }));
+      });
+      if (entryWithBadCov) {
+        return 'The covariance matrix of criterion: "' + entryWithBadCov.criterion + '" refers to nonexistent alternative: "' + alternativeKey + '"';
+      }
+    }
+
     return {
       getObservedScales: getObservedScales,
       buildTheoreticalScales: buildTheoreticalScales,
@@ -180,7 +452,8 @@ define(['lodash', 'angular'], function(_) {
       buildAggregateState: buildAggregateState,
       mergeBaseAndSubProblem: mergeBaseAndSubProblem,
       setDefaultObservedScales: setDefaultObservedScales,
-      filterScenariosWithResults: filterScenariosWithResults
+      filterScenariosWithResults: filterScenariosWithResults,
+      validateWorkspace: validateWorkspace
     };
   };
 
--- frontend-test/unit/workspaceServiceSpec.js
+++ frontend-test/unit/workspaceServiceSpec.js
@@ -1,5 +1,5 @@
 'use strict';
-define(['angular', 'angular-mocks', 'mcda/workspace/workspace'], function() {
+define(['lodash', 'angular', 'angular-mocks', 'mcda/workspace/workspace', 'mcda/misc'], function(_) {
   describe('The WorkspaceService, ', function() {
     var workspaceService;
     beforeEach(module('elicit.workspace', function($provide) {
@@ -408,7 +408,7 @@ define(['angular', 'angular-mocks', 'mcda/workspace/workspace'], function() {
         };
         var expectedResult = {
           criteria: {
-            crit1: { },
+            crit1: {},
             crit2: {
               scale: [0, 1],
               pvf: 'pvf'
@@ -511,6 +511,330 @@ define(['angular', 'angular-mocks', 'mcda/workspace/workspace'], function() {
           }
         }];
         expect(result).toEqual(expectedResult);
+      });
+    });
+
+    describe('validateWorkspace', function() {
+      it('should indicate which required properties are missing', function() {
+        var empty = {};
+        var validity = workspaceService.validateWorkspace(empty);
+        expect(validity.isValid).toBeFalsy();
+        expect(validity.errorMessage).toBe('Missing workspace properties: title, criteria, alternatives, performanceTable');
+      });
+      it('should fail gracefully when exceptions occur', function () {
+        var garbage = {
+          title: 'foo',
+          criteria: {
+            foo: {
+              title: 'bar'
+            },
+            qux: {
+              title: 'fnord'
+            }
+          },
+          alternatives: {
+            foo: {
+              title: 'bar'
+            },
+            qux: {
+              title: 'fnord'
+            }
+          },
+          performanceTable: '5'
+        };
+        var validity = workspaceService.validateWorkspace(garbage);
+        expect(validity.isValid).toBeFalsy();
+        expect(validity.errorMessage).toBe('Exception while reading problem. Please make sure the file follows the specifications as laid out in the manual');
+      });
+      describe('for absolute performances', function() {
+        it('should return valid for a valid problem', function() {
+          var example = exampleProblem();
+          var validity = workspaceService.validateWorkspace(example);
+          expect(validity.isValid).toBeTruthy();
+          expect(validity.errorMessage).toBe(undefined);
+        });
+        it('should fail if a required property is missing', function() {
+          var example = exampleProblem();
+          var withoutTitle = _.omit(example, 'title');
+          var validityWithoutTitle = workspaceService.validateWorkspace(withoutTitle);
+          expect(validityWithoutTitle.isValid).toBeFalsy();
+          expect(validityWithoutTitle.errorMessage).toBe("Missing workspace property: title");
+          var withoutCriteriaAndAlternatives = _.omit(example, ['criteria', 'alternatives']);
+          var validitywithoutCriteriaAndAlternatives = workspaceService.validateWorkspace(withoutCriteriaAndAlternatives);
+          expect(validitywithoutCriteriaAndAlternatives.isValid).toBeFalsy();
+          expect(validitywithoutCriteriaAndAlternatives.errorMessage).toBe("Missing workspace properties: criteria, alternatives");
+        });
+        it('should fail if there are fewer than 2 criteria', function() {
+          var example = exampleProblem();
+          var exampleWithOneCriterion = _.cloneDeep(example);
+          delete exampleWithOneCriterion.criteria.Bleed;
+          delete exampleWithOneCriterion.criteria['Prox DVT'];
+          var validity = workspaceService.validateWorkspace(exampleWithOneCriterion);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Two or more criteria required');
+        });
+        it('should fail if there are fewer than 2 alternatives', function() {
+          var example = exampleProblem();
+          var exampleWithOneAlternative = _.cloneDeep(example);
+          delete exampleWithOneAlternative.alternatives.Hep;
+          var validity = workspaceService.validateWorkspace(exampleWithOneAlternative);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Two or more alternatives required');
+        });
+        it('should fail if there is data in the performanceTable for nonexistent criteria', function() {
+          var exampleWithExtraPerformanceData = _.cloneDeep(exampleProblem());
+          exampleWithExtraPerformanceData.performanceTable.push({
+            alternative: 'Enox',
+            criterion: 'nonsense',
+            performance: {
+              type: 'dbeta',
+              parameters: { alpha: 5, beta: 124 }
+            }
+          });
+          var validity = workspaceService.validateWorkspace(exampleWithExtraPerformanceData);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Performance table contains data for nonexistent criterion: "nonsense"');
+        });
+        it('should fail if there is data in the performanceTable for nonexistent alternatives', function() {
+          var exampleWithExtraPerformanceData = _.cloneDeep(exampleProblem());
+          exampleWithExtraPerformanceData.performanceTable.push({
+            alternative: 'nonsense',
+            criterion: 'Bleed',
+            performance: {
+              type: 'dbeta',
+              parameters: { alpha: 5, beta: 124 }
+            }
+          });
+          var validity = workspaceService.validateWorkspace(exampleWithExtraPerformanceData);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Performance table contains data for nonexistent alternative: "nonsense"');
+        });
+        it('should fail if a cell of the performance table is left empty', function() {
+          var exampleWithMissingPerformanceData = _.cloneDeep(exampleProblem());
+          exampleWithMissingPerformanceData.performanceTable.pop();
+          var validity = workspaceService.validateWorkspace(exampleWithMissingPerformanceData);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Performance table is missing data for criterion "Bleed" and alternative "Enox"');
+        });
+        it('should fail if a criterion lacks a title', function() {
+          var missingCriterionTitle = _.cloneDeep(exampleProblem());
+          delete missingCriterionTitle.criteria.Bleed.title;
+          var validity = workspaceService.validateWorkspace(missingCriterionTitle);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Missing title for criterion: "Bleed"');
+        });
+        it('should fail if a alternative lacks a title', function() {
+          var missingAlternativeTitle = _.cloneDeep(exampleProblem());
+          delete missingAlternativeTitle.alternatives.Hep.title;
+          var validity = workspaceService.validateWorkspace(missingAlternativeTitle);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Missing title for alternative: "Hep"');
+        });
+      });
+      describe('for relative performances', function() {
+        it('should work for a valid problem', function() {
+          var problem = exampleRelativeProblem();
+          var validity = workspaceService.validateWorkspace(problem);
+          expect(validity.isValid).toBeTruthy();
+          expect(validity.errorMessage).toBe(undefined);
+        });
+        it('should fail when the baseline is missing', function() {
+          var problemWithMissingBaseline = _.cloneDeep(exampleRelativeProblem());
+          delete problemWithMissingBaseline.performanceTable[0].performance.parameters.baseline;
+          var validity = workspaceService.validateWorkspace(problemWithMissingBaseline);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Missing baseline for criterion: "crit1"');
+        });
+        it('should fail when the mu refers to a nonexistent alternative', function() {
+          var problemWithNonsenseMu = _.cloneDeep(exampleRelativeProblem());
+          var mu = problemWithNonsenseMu.performanceTable[0].performance.parameters.relative.mu;
+          delete mu['4939'];
+          mu.nonsense = 3;
+          var validity = workspaceService.validateWorkspace(problemWithNonsenseMu);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('The mu of the performance of criterion: "crit1" refers to nonexistent alternative: "nonsense"');
+        });
+        it('should fail when the cov rownames or colnames refer to a nonexistent alternative', function() {
+          var problemWithNonsenseRowName = _.cloneDeep(exampleRelativeProblem());
+          var cov = problemWithNonsenseRowName.performanceTable[0].performance.parameters.relative.cov;
+          cov.rownames[0] = 'nonsense';
+          var validity = workspaceService.validateWorkspace(problemWithNonsenseRowName);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('The covariance matrix of criterion: "crit1" refers to nonexistent alternative: "nonsense"');
+          var problemWithNonsenseColName = _.cloneDeep(exampleRelativeProblem());
+          cov = problemWithNonsenseColName.performanceTable[0].performance.parameters.relative.cov;
+          cov.colnames[0] = 'nonsense';
+          validity = workspaceService.validateWorkspace(problemWithNonsenseColName);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('The covariance matrix of criterion: "crit1" refers to nonexistent alternative: "nonsense"');
+        });
+      });
+
+      describe('regarding preferences', function() {
+        it('should fail if a preference refers to a nonexistent criterion', function() {
+          var exampleWithBadPreference = _.cloneDeep(exampleProblem());
+          exampleWithBadPreference.preferences = [{
+            type: 'ordinal',
+            criteria: ['Bleed', 'Prox DVT']
+          }, {
+            type: 'ordinal',
+            criteria: ['Prox DVT', 'nonsense']
+          }];
+          var validity = workspaceService.validateWorkspace(exampleWithBadPreference);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Preferences contain data for nonexistent criterion: "nonsense"');
+        });
+        it('should fail if there are mixed preferences', function() {
+          var preferencesMixed = _.cloneDeep(exampleProblem());
+          preferencesMixed.preferences = [{
+            type: 'ordinal',
+            criteria: ['Bleed', 'Prox DVT']
+          }, {
+            type: 'exact swing',
+            criteria: ['Prox DVT', 'Dist DVT'],
+            ratio: 1.5
+          }];
+          var validity = workspaceService.validateWorkspace(preferencesMixed);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Preferences should all be the same type');
+        });
+        it('should not fail if ordinal preferences are fine', function() {
+          var consistentOrdinal = _.cloneDeep(exampleProblem());
+          consistentOrdinal.preferences = [{
+            type: 'ordinal',
+            criteria: ['Bleed', 'Prox DVT']
+          }, {
+            type: 'ordinal',
+            criteria: ['Prox DVT', 'Dist DVT']
+          }];
+          var validity = workspaceService.validateWorkspace(consistentOrdinal);
+          expect(validity.isValid).toBeTruthy();
+          expect(validity.errorMessage).toBe(undefined);
+        });
+        it('should fail if ordinal preferences are inconsistent', function() {
+          var inconsistentOrdinalWithTree = _.cloneDeep(exampleProblem());
+          inconsistentOrdinalWithTree.preferences = [{
+            type: 'ordinal',
+            criteria: ['Bleed', 'Prox DVT']
+          }, {
+            type: 'ordinal',
+            criteria: ['Bleed', 'Dist DVT']
+          }];
+          var validity = workspaceService.validateWorkspace(inconsistentOrdinalWithTree);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Inconsistent ordinal preferences');
+          var inconsistentOrdinalCycle = _.cloneDeep(exampleProblem());
+          inconsistentOrdinalCycle.preferences = [{
+            type: 'ordinal',
+            criteria: ['Bleed', 'Prox DVT']
+          }, {
+            type: 'ordinal',
+            criteria: ['Prox DVT', 'Bleed']
+          }];
+          validity = workspaceService.validateWorkspace(inconsistentOrdinalCycle);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Inconsistent ordinal preferences');
+          var inconsistentOrdinalTooLong = _.cloneDeep(exampleProblem());
+          inconsistentOrdinalTooLong.preferences = [{
+            type: 'ordinal',
+            criteria: ['Bleed', 'Prox DVT']
+          }, {
+            type: 'ordinal',
+            criteria: ['Prox DVT', 'Bleed']
+          }, {
+            type: 'ordinal',
+            criteria: ['Bleed', 'Prox DVT']
+          }];
+          validity = workspaceService.validateWorkspace(inconsistentOrdinalTooLong);
+          expect(validity.isValid).toBeFalsy();
+          var inconsistentOrdinalTooShort = _.cloneDeep(exampleProblem());
+          inconsistentOrdinalTooShort.preferences = [{
+            type: 'ordinal',
+            criteria: ['Bleed', 'Prox DVT']
+          }];
+          validity = workspaceService.validateWorkspace(inconsistentOrdinalTooShort);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Inconsistent ordinal preferences');
+          var inconsistentOrdinalSelfReference = _.cloneDeep(exampleProblem());
+          inconsistentOrdinalSelfReference.preferences = [{
+            type: 'ordinal',
+            criteria: ['Bleed', 'Bleed']
+          }, {
+            type: 'ordinal',
+            criteria: ['Bleed', 'Prox DVT']
+          }];
+          validity = workspaceService.validateWorkspace(inconsistentOrdinalSelfReference);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Inconsistent ordinal preferences');
+        });
+        it('should succeed on consistent exact preferences', function() {
+          var consistentExact = _.cloneDeep(exampleProblem());
+          consistentExact.preferences = [{
+            type: 'exact swing',
+            criteria: ['Bleed', 'Prox DVT'],
+            ratio: 1
+          }, {
+            type: 'exact swing',
+            criteria: ['Bleed', 'Dist DVT'],
+            ratio: 1
+          }];
+          var validity = workspaceService.validateWorkspace(consistentExact);
+          expect(validity.isValid).toBeTruthy();
+        });
+        it('should fail on inconsistent exact preferences', function() {
+          var inconsistentWrongRoot = _.cloneDeep(exampleProblem());
+          inconsistentWrongRoot.preferences = [{
+            type: 'exact swing',
+            criteria: ['Bleed', 'Prox DVT'],
+            ratio: 1
+          }, {
+            type: 'exact swing',
+            criteria: ['Prox DVT', 'Dist DVT'],
+            ratio: 1
+          }];
+          var validity = workspaceService.validateWorkspace(inconsistentWrongRoot);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Inconsistent exact weighting preferences');
+          var inconsistentBadRatio = _.cloneDeep(exampleProblem());
+          inconsistentBadRatio.preferences = [{
+            type: 'exact swing',
+            criteria: ['Bleed', 'Prox DVT'],
+            ratio: 1.5
+          }, {
+            type: 'exact swing',
+            criteria: ['Bleed', 'Dist DVT'],
+            ratio: 1
+          }];
+          validity = workspaceService.validateWorkspace(inconsistentBadRatio);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Inconsistent exact weighting preferences');
+          var inconsistentNegative = _.cloneDeep(exampleProblem());
+          inconsistentNegative.preferences = [{
+            type: 'exact swing',
+            criteria: ['Bleed', 'Prox DVT'],
+            ratio: -0
+          }, {
+            type: 'exact swing',
+            criteria: ['Bleed', 'Dist DVT'],
+            ratio: 1
+          }];
+          validity = workspaceService.validateWorkspace(inconsistentNegative);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Inconsistent exact weighting preferences');
+          var inconsistentSelfReference = _.cloneDeep(exampleProblem());
+          inconsistentSelfReference.preferences = [{
+            type: 'exact swing',
+            criteria: ['Bleed', 'Bleed'],
+            ratio: 1
+          }, {
+            type: 'exact swing',
+            criteria: ['Prox DVT', 'Dist DVT'],
+            ratio: 1
+          }];
+          validity = workspaceService.validateWorkspace(inconsistentSelfReference);
+          expect(validity.isValid).toBeFalsy();
+          expect(validity.errorMessage).toBe('Inconsistent exact weighting preferences');
+        });
       });
     });
   });
