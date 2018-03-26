59f86de85fd2bf2b1bc256c8b42e95bd13e5be49
Daan Reid <osmosisch@gmail.com>
2018-03-26 16:51:54 +0200

    Refactor index; add helmet; work towards SSL version

---
+++ node-backend/orderingService.js
@@ -0,0 +1,35 @@
+'use strict';
+var logger = require('./logger');
+
+module.exports = function(db) {
+  function getOrdering(req, res, next) {
+    logger.debug('GET /workspaces/' + req.params.workspaceId + '/ordering');
+    db.query('SELECT workspaceId AS "workspaceId", ordering FROM ordering WHERE workspaceId = $1', [req.params.workspaceId], function(err, result) {
+      if (err) {
+        err.status = 500;
+        return next(err);
+      }
+      res.json({
+        ordering: result.rows[0] ? result.rows[0].ordering : undefined
+      });
+    });
+  }
+
+  function updateOrdering(req, res, next) {
+    logger.debug('SET /workspaces/' + req.params.workspaceId + '/ordering/');
+    db.query('insert into ordering(workspaceId, ordering) values($1, $2) ON CONFLICT(workspaceId) DO UPDATE SET ordering=$2', [
+      req.params.workspaceId,
+      req.body
+    ], function(err) {
+      if (err) {
+        err.status = 500;
+        next(err);
+      }
+      res.end();
+    });
+  }
+  return {
+    getOrdering: getOrdering,
+    updateOrdering: updateOrdering
+  }
+}
---
+++ node-backend/userManagement.js
@@ -0,0 +1,66 @@
+'use strict';
+module.exports = function(db) {
+
+  function findOrCreateUser(sess, accessToken, extra, googleUser) {
+    var user = this.Promise();
+
+    function userTransaction(client, callback) {
+      client.query("SELECT id, username, firstName, lastName FROM UserConnection LEFT JOIN Account ON UserConnection.userid = Account.username WHERE providerUserId = $1 AND providerId = 'google'", [googleUser.id], function(error, result) {
+        if (error) {
+          return callback(error);
+        }
+        if (result.rows.length === 0) {
+          client.query("INSERT INTO UserConnection (userId, providerId, providerUserId, rank, displayName, profileUrl, accessToken, refreshToken, expireTime)" +
+            " VALUES ($1, 'google', $2, 1, $3, $4, $5, $6, $7)", [googleUser.id, googleUser.id, googleUser.name, googleUser.link, accessToken, extra.refresh_token, extra.expires_in],
+            function(error) {
+              if (error) {
+                return callback(error);
+              }
+              client.query("INSERT INTO Account (username, firstName, lastName) VALUES ($1, $2, $3) RETURNING id", [googleUser.id, googleUser.given_name, googleUser.family_name],
+                function(error, result) {
+                  if (error) {
+                    return callback(error);
+
+                  }
+                  var row = result.rows[0];
+                  return callback(null, {
+                    'id': row.id,
+                    'username': googleUser.id,
+                    'firstName': googleUser.given_name,
+                    'lastName': googleUser.family_name
+                  });
+                });
+            });
+        } else {
+          callback(null, result.rows[0]);
+        }
+      });
+    }
+
+    db.runInTransaction(userTransaction, function(error, result) {
+      if (error) {
+        return user.fail(error);
+      }
+      user.fulfill(result);
+    });
+
+    return user;
+  }
+
+  function findUserById(id, callback) {
+    db.query('SELECT id, username, firstName, lastName FROM Account WHERE id = $1', [id], function(error, result) {
+      if (error) {
+        callback(error);
+      } else if (result.rows.length === 0) {
+        callback('ID ' + id + ' not found');
+      } else {
+        callback(null, result.rows[0]);
+      }
+    });
+  }
+
+  return {
+    findOrCreateUser: findOrCreateUser,
+    findUserById: findUserById
+  };
+};
