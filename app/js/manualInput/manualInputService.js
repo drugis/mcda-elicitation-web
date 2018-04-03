7feea267e453dde37dbccff1eb39a1acf8678861
Joris de Keijser <chinook25@gmail.com>
2018-04-03 09:55:54 +0200

    split strength of evidence and uncertainties on input

--- app/js/manualInput/manualInputService.js
+++ app/js/manualInput/manualInputService.js
@@ -346,7 +346,7 @@ define(['lodash', 'angular'], function(_) {
 
     function copyWorkspaceCriteria(workspace) {
       return _.map(workspace.problem.criteria, function(criterion, key) {
-        var newCrit = _.pick(criterion, ['title', 'description', 'source', 'sourceLink', 'unitOfMeasurement', 'strengthOfEvidence']);
+        var newCrit = _.pick(criterion, ['title', 'description', 'source', 'sourceLink', 'unitOfMeasurement', 'strengthOfEvidence', 'uncertainties']);
         if (workspace.problem.valueTree) {
           newCrit.isFavorable = _.includes(workspace.problem.valueTree.children[0].criteria, key) ? true : false;
         }
