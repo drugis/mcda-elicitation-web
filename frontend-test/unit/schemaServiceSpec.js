c85f349c6d616ed7a92ec3e0f76369df2fff8a1a
Joris de Keijser <chinook25@gmail.com>
2018-05-28 16:54:45 +0200

    add files

---
+++ frontend-test/unit/schemaServiceSpec.js
@@ -0,0 +1,76 @@
+'use strict';
+define(['angular-mocks', 'mcda/benefitRisk/benefitRisk'], function() {
+
+  var generateUuidMock = jasmine.createSpy('generateUuid');
+  var currentSchemaVersion = '1.0.0';
+  var schemaService;
+
+  describe('The SchemaService', function() {
+    beforeEach(module('elicit.benefitRisk', function($provide) {
+      $provide.value('generateUuid', generateUuidMock);
+      $provide.value('currentSchemaVersion', currentSchemaVersion);
+    }));
+
+    beforeEach(inject(function(SchemaService) {
+      schemaService = SchemaService;
+    }));
+
+    describe('updateProblemToCurrentSchema', function() {
+      it('it should do nothing to a workspace of the current version', function() {
+        var workspace = {
+          problem: {
+            criteria: {
+              crit1: {
+                title: 'criterion 1',
+                description: 'desc',
+                unitOfMeasurement: 'ms',
+                uncertainties: 'unc',
+                source: 'source'
+              }
+            },
+            performanceTable: [{
+              criterion: 'crit1',
+              alternative: 'alt1',
+              performance: { type: 'dnorm' }
+            }]
+          }
+        };
+        var result = schemaService.updateProblemToCurrentSchema(workspace);
+        var expectedResult = {
+          problem: {
+            criteria: {
+              crit1: {
+                title: 'criterion 1',
+                description: 'desc',
+                unitOfMeasurement: 'ms',
+                dataSources: [{
+                  id: undefined,
+                  inputType: 'distribution',
+                  inputMethod: 'manualDistribution',
+                  uncertainties: 'unc',
+                  source: 'source'
+                }]
+              }
+            },
+            performanceTable: [{
+              criterion: 'crit1',
+              alternative: 'alt1',
+              dataSource: undefined,
+              performance: { type: 'dnorm' }
+            }]
+          },
+          schemaVersion: '1.0.0'
+        };
+        expect(result).toEqual(expectedResult);
+      });
+      it('it should update a workspace without schemaversion to the current version', function() {
+        var workspace = {
+          version: '1.0.0',
+          problem: exampleProblem()
+        };
+        var result = schemaService.updateProblemToCurrentSchema(workspace);
+        expect(result).toEqual(workspace);
+      });
+    });
+  });
+});
