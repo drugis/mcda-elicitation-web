662e76aa81ede3bfda4f4ce3791d573d31adda7a
Joris de Keijser <chinook25@gmail.com>
2018-06-22 11:18:02 +0200

    toggle between percentage and decimal

--- app/js/effectsTable/effectsTableScalesCellDirective.js
+++ app/js/effectsTable/effectsTableScalesCellDirective.js
@@ -1,18 +1,43 @@
 'use strict';
 define([], function() {
 
-  var dependencies = [];
+  var dependencies = ['$filter'];
 
-  var EffectsTableScalesCellDirective = function() {
+  var EffectsTableScalesCellDirective = function($filter) {
     return {
       restrict: 'E',
       scope: {
         'scales': '=',
-        'uncertainty': '='
+        'uncertainty': '=',
+        'showPercentage': '=',
+        'canBePercentage': '='
       },
-      template: '<div>{{scales[\'50%\'] | number}}</div>' +
-        '<div class="uncertain" ng-show="uncertainty">{{scales[\'2.5%\'] | number}}, {{scales[\'97.5%\'] | number}}</div>',
-      link: function() {}
+      template: '<div>{{median}}</div>' +
+        '<div class="uncertain" ng-show="uncertainty">{{lowerBound}}, {{upperBound}}</div>',
+      link: function(scope) {
+        scope.$watch('scales', initScales);
+        scope.$watch('showPercentage', initScales);
+
+        function initScales() {
+          if (scope.scales) {
+            scope.lowerBound = getRoundedValue(scope.scales['2.5%']);
+            scope.median = getRoundedValue(scope.scales['50%']);
+            scope.upperBound = getRoundedValue(scope.scales['97.5%']);
+          }
+        }
+        function getRoundedValue(value) {
+          if (value === null) { return; }
+          if(!scope.canBePercentage) {return $filter('number')(value);}
+          var numberOfDecimals = 1;
+          if (Math.abs(value) < 0.01) {
+            ++numberOfDecimals;
+          }
+          if (!scope.showPercentage && Math.abs(value) < 1) {
+            numberOfDecimals += 2;
+          }
+          return $filter('number')(scope.showPercentage ? value * 100 : value, numberOfDecimals);
+        }
+      }
     };
   };
   return dependencies.concat(EffectsTableScalesCellDirective);
