8fff376a3b4538ab66ac2baeb205a5930f51df98
Joris de Keijser <chinook25@gmail.com>
2018-06-22 12:48:35 +0200

    try chartjs

--- app/js/preferences/willingnessToTradeOffChartDirective.js
+++ app/js/preferences/willingnessToTradeOffChartDirective.js
@@ -1,6 +1,6 @@
 'use strict';
-define(['lodash', 'd3', 'nvd3', 'jQuery'],
-  function(_, d3, nv, $) {
+define(['chartjs', 'jQuery'],
+  function(Chart, $) {
     var dependencies = [
 
     ];
@@ -12,42 +12,72 @@ define(['lodash', 'd3', 'nvd3', 'jQuery'],
         scope: {
           settings: '='
         },
-        template: '<div id="willingnesschart"></div>',
+        template: '<div><canvas id="willingnessChart" width="400" height="400"></canvas></div>',
         link: function(scope, element) {
-          var dim = getParentDimension(element);
-          var svg = d3.select(element[0]).append('svg')
-            .attr('width', '100%')
-            .attr('height', '100%');
-          nv.addGraph(function() {
-            var data = [
-              {
-                values: [{ x: 5, y: 5 },{ x: 4, y: 4 },{ x: 3, y: 3 },{ x: 2, y: 2 }, { x: 1, y: 1 }],
-                key: 'test',
-                color: 'black'
-              },
-              {
-                values: [{ x: 3, y: 3, shape: 'diamond'}],
-                key: 'test2 ',
-                color: 'red'
-              }
-            ];
-            var chart = nv.models.lineChart()
-              .height(dim.height)
-              .width(dim.width)
-              .useVoronoi(true)
-              .interpolate('linear')
-              .pointActive(function(dot) {
-                return dot.shape === 'diamond';
-              })
-              .duration(300)
-              .color(d3.scale.category10().range());
-            chart.xAxis.tickFormat(d3.format(',.3g'));
-            chart.yAxis.tickFormat(d3.format(',.3g'));
-            svg.datum(data).call(chart);
-            nv.utils.windowResize(chart.update);
+          scope.$watch('settings', function(newSettings){
+            if(newSettings){
+              var ctx = document.getElementById('willingnessChart').getContext('2d');
+              new Chart(ctx, {
+                type: 'scatter',
+                data: {
+                  labels: [],
+                  datasets: []
+                },
+                options: {
+                  scales: {
+                    yAxes: [{
+                      ticks: {
+                        min: scope.settings.secondCriterion.dataSources[0].pvf.range[0],
+                        max: scope.settings.secondCriterion.dataSources[0].pvf.range[1]
+                      }
+                    }],
+                    xAxes: [{
+                      ticks: {
+                        min: scope.settings.firstCriterion.dataSources[0].pvf.range[0],
+                        max: scope.settings.firstCriterion.dataSources[0].pvf.range[1]
+                      }
+                    }]
+                  }
+                }
+              });
+            }
           });
-          svg.selectAll('svg .nv-lineChart .nv-point').style('stroke-width',
-          '7px').style('fill-opacity', '.95').style('stroke-opacity', '.95');
+          var dim = getParentDimension(element);
+
+
+          // var svg = d3.select(element[0]).append('svg')
+          //   .attr('width', '100%')
+          //   .attr('height', '100%');
+          // nv.addGraph(function() {
+          //   var data = [
+          //     {
+          //       values: [{ x: 5, y: 5 },{ x: 4, y: 4 },{ x: 3, y: 3 },{ x: 2, y: 2 }, { x: 1, y: 1 }],
+          //       key: 'test',
+          //       color: 'black'
+          //     },
+          //     {
+          //       values: [{ x: 3, y: 3, shape: 'diamond'}],
+          //       key: 'test2 ',
+          //       color: 'red'
+          //     }
+          //   ];
+          //   var chart = nv.models.lineChart()
+          //     .height(dim.height)
+          //     .width(dim.width)
+          //     .useVoronoi(true)
+          //     .interpolate('linear')
+          //     .pointActive(function(dot) {
+          //       return dot.shape === 'diamond';
+          //     })
+          //     .duration(300)
+          //     .color(d3.scale.category10().range());
+          //   chart.xAxis.tickFormat(d3.format(',.3g'));
+          //   chart.yAxis.tickFormat(d3.format(',.3g'));
+          //   svg.datum(data).call(chart);
+          //   nv.utils.windowResize(chart.update);
+          // });
+          // svg.selectAll('svg .nv-lineChart .nv-point').style('stroke-width',
+          // '7px').style('fill-opacity', '.95').style('stroke-opacity', '.95');
 
 
           function getParentDimension(element) {
--- app/js/preferences/willingnessToTradeOffDirective.js
+++ app/js/preferences/willingnessToTradeOffDirective.js
@@ -16,15 +16,15 @@ define(['lodash'],
       link: function(scope) {
         scope.criteria = decorateWithId(scope.problem.criteria);
         scope.settings = {
-          firstCriterion: scope.criteria[0].id,
-          secondCriterion: scope.criteria[1].id
+          firstCriterion: scope.criteria[0],
+          secondCriterion: scope.criteria[1]
         };
         firstCriterionChanged();
 
         scope.firstCriterionChanged = firstCriterionChanged;
         function firstCriterionChanged() {
           scope.filteredCriteria = _.reject(scope.criteria, ['id', scope.settings.firstCriterion]);
-          scope.settings.secondCriterion = scope.filteredCriteria[0].id;
+          scope.settings.secondCriterion = scope.filteredCriteria[0];
         }
         function decorateWithId(list) {
           return _.map(list, function(item, id) {
