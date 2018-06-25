5f0eafbbf04f923f73f6f3ddfcf50863bc7d1033
Joris de Keijser <chinook25@gmail.com>
2018-06-25 16:53:28 +0200

    wip

--- app/js/preferences/willingnessToTradeOffChartDirective.js
+++ app/js/preferences/willingnessToTradeOffChartDirective.js
@@ -1,6 +1,6 @@
 'use strict';
-define(['d3', 'nvd3'],
-  function(d3, nvd3) {
+define(['c3', 'd3'],
+  function(c3, d3) {
     var dependencies = [
       'mcdaRootPath',
       '$timeout'
@@ -16,13 +16,14 @@ define(['d3', 'nvd3'],
         },
         templateUrl: mcdaRootPath + 'js/preferences/willingnessToTradeOffChartDirective.html',
         link: function(scope, element) {
-          var firstPointData;
-          var chart;
-          var data;
           scope.updateFirstPoint = updateFirstPoint;
 
           scope.coordinates = {};
-
+          var chart;
+          var data = {
+            xs: { firstPoint: 'firstPoint_x' },
+            columns: []
+          };
           var root = d3.select($(element).get(0));
           root = root.select('svg');
 
@@ -34,69 +35,75 @@ define(['d3', 'nvd3'],
 
 
           function initChart(newSettings) {
-            firstPointData = {
-              values: [{x:0.5, y:0.6}]
-            };
-            var minX = newSettings.firstCriterion.dataSources[0].pvf.range[0];
-            var maxX = newSettings.firstCriterion.dataSources[0].pvf.range[1];
-            var minY = newSettings.secondCriterion.dataSources[0].pvf.range[0];
-            var maxY = newSettings.secondCriterion.dataSources[0].pvf.range[1];
-            chart = nvd3.models.lineChart()
-              .showLegend(true)
-              .useInteractiveGuideline(true)
-              .color(d3.scale.category10().range())
-              .xDomain([minX, maxX])
-              .yDomain([maxY, minY]);
-            //Axis settings
-            chart.xAxis.axisLabel(newSettings.firstCriterion.title);
-            chart.yAxis.axisLabel(newSettings.secondCriterion.title);
-            chart.xAxis.tickFormat(d3.format('.02f'));
-            chart.yAxis.tickFormat(d3.format('.02f'));
-            data = [{
-              values: [
-                { x: 0.1, y: 0.8 },
-                { x: 0.5, y: 0.5 },
-                { x: 0.7, y: 0.2 },
-              ]
-            }, firstPointData];
             root.append('rect')
               .attr('width', '100%')
               .attr('height', '100%')
               .attr('fill', 'white');
-
             root
               .style('width', '500px')
               .style('height', '500px')
-              .style('background', 'white')
-              .datum(data)
-              .call(chart);
+              .style('background', 'white');
+            scope.coordinates = {};
+            var minX = newSettings.firstCriterion.dataSources[0].pvf.range[0];
+            var maxX = newSettings.firstCriterion.dataSources[0].pvf.range[1];
+            var minY = newSettings.secondCriterion.dataSources[0].pvf.range[0];
+            var maxY = newSettings.secondCriterion.dataSources[0].pvf.range[1];
 
-            var background = root.select('.nv-focus .nv-background rect');
-            background.on('click', clickHandler);
+            var chart = c3.generate({
+              bindto: root,
+              data: {
+                xs: { firstPoint: 'firstPoint_x' },
+                columns: []
+              },
+              type: 'scatter',
+              axis: {
+                x: {
+                  ticks: 10,
+                  min: minX,
+                  max: maxX,
+                  label: newSettings.firstCriterion.title,
+                  padding: {
+                    left: 0,
+                    right: 0
+                  }
+                },
+                y: {
+                  ticks: 10,
+                  min: minY,
+                  max: maxY,
+                  label: newSettings.secondCriterion.title,
+                  padding: {
+                    top: 0,
+                    bottom: 0
+                  }
+                }
+              }
+            });
+
+
+            // var background = root.select('.nv-focus .nv-background rect');
+            chart.internal.main.on('click', clickHandler); // https://github.com/c3js/c3/issues/705
 
             function clickHandler() {
-
               var coords = d3.mouse(this);
 
-              var xScale = chart.xAxis.scale();
-              var yScale = chart.yAxis.scale();
-              firstPointData.values[0] = {
-                x: xScale.invert(coords[0]),  // Takes the pixel number to convert to number
-                y: yScale.invert(coords[1])
-              };
-              scope.coordinates = firstPointData.values[0];
-              $timeout(function() {
-                chart.update();
-              });
+              scope.coordinates.x = chart.internal.x.invert(coords[0]);
+              scope.coordinates.y = chart.internal.y.invert(coords[1]);
+
+              data.columns[0] = ['firstPoint', scope.coordinates.y];
+              data.columns[1] = ['firstPoint_x', scope.coordinates.x];
+              chart.load(data);
+              $timeout(); // force coordinate update outside chart
             }
 
           }
 
           function updateFirstPoint() {
-            firstPointData.values = scope.coordinates;
-            $timeout(function() {
-              chart.update();
-            });
+            if (scope.coordinates.x > -Infinity && scope.coordinates.y > -Infinity) {
+              data.columns[0] = ['firstPoint', scope.coordinates.y];
+              data.columns[1] = ['firstPoint_x', scope.coordinates.x];
+              chart.load(data);
+            }
           }
 
         }
