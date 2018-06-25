5afb68f363da4b8ccc842755ff541435b43a15cd
Joris de Keijser <chinook25@gmail.com>
2018-06-25 12:34:04 +0200

    wip

--- app/js/preferences/willingnessToTradeOffChartDirective.js
+++ app/js/preferences/willingnessToTradeOffChartDirective.js
@@ -1,13 +1,13 @@
 'use strict';
-define(['chartjs', 'jQuery'],
-  function(Chart, $) {
+define(['d3', 'nvd3'],
+  function(d3, nvd3) {
     var dependencies = [
-      '$timeout',
-      'mcdaRootPath'
+      'mcdaRootPath',
+      '$timeout'
     ];
     var WillingnessToTradeOffChartDirective = function(
-      $timeout,
-      mcdaRootPath
+      mcdaRootPath,
+      $timeout
     ) {
       return {
         restrict: 'E',
@@ -15,98 +15,90 @@ define(['chartjs', 'jQuery'],
           settings: '='
         },
         templateUrl: mcdaRootPath + 'js/preferences/willingnessToTradeOffChartDirective.html',
-        link: function(scope) {
-
-          scope.updateChart = updateChart;
+        link: function(scope, element) {
+          var firstPointData;
+          var chart;
+          var data;
+          scope.updateFirstPoint = updateFirstPoint;
 
           scope.coordinates = {};
 
+          var root = d3.select($(element).get(0));
+          root = root.select('svg');
+
           scope.$watch('settings', function(newSettings) {
             if (newSettings) {
-              var ctx = document.getElementById('willingnessChart').getContext('2d');
-              scope.willingnessChart = new Chart(ctx, {
-                type: 'scatter',
-                data: {
-                  datasets: [{
-                    label: 'First point',
-                    fill: false,
-                    data: []
-                  }]
-                },
-                options: {
-                  scales: {
-                    yAxes: [{
-                      ticks: {
-                        min: scope.settings.secondCriterion.dataSources[0].pvf.range[0],
-                        max: scope.settings.secondCriterion.dataSources[0].pvf.range[1]
-                      }
-                    }],
-                    xAxes: [{
-                      ticks: {
-                        min: scope.settings.firstCriterion.dataSources[0].pvf.range[0],
-                        max: scope.settings.firstCriterion.dataSources[0].pvf.range[1]
-                      }
-                    }]
-                  },
-                  onClick: function(evt) {
-                    // console.log(evt.offsetX + ',' + evt.offsetY);
-                    var yTop = scope.willingnessChart.chartArea.top;
-                    var yBottom = scope.willingnessChart.chartArea.bottom;
-                    scope.coordinates.yMin = scope.willingnessChart.scales['y-axis-1'].min;
-                    scope.coordinates.yMax = scope.willingnessChart.scales['y-axis-1'].max;
-                    var newY = '';
-                    var showstuff = 0;
-                    if (evt.offsetY <= yBottom && evt.offsetY >= yTop) {
-                      newY = Math.abs((evt.offsetY - yTop) / (yBottom - yTop));
-                      newY = (newY - 1) * -1;
-                      newY = newY * (Math.abs(scope.coordinates.yMax - scope.coordinates.yMin)) + scope.coordinates.yMin;
-                      showstuff = 1;
-                    }
-                    var xTop = scope.willingnessChart.chartArea.left;
-                    var xBottom = scope.willingnessChart.chartArea.right;
-                    scope.coordinates.xMin = scope.willingnessChart.scales['x-axis-1'].min;
-                    scope.coordinates.xMax = scope.willingnessChart.scales['x-axis-1'].max;
-                    var newX = '';
-                    if (evt.offsetX <= xBottom && evt.offsetX >= xTop && showstuff === 1) {
-                      newX = Math.abs((evt.offsetX - xTop) / (xBottom - xTop));
-                      newX = newX * (Math.abs(scope.coordinates.xMax - scope.coordinates.xMin)) + scope.coordinates.xMin;
-                    }
-                    if (newY !== '' && newX !== '') {
-                      // console.log(newX + ',' + newY);
-                      scope.coordinates.x = newX;
-                      scope.coordinates.y = newY;
-                      updateChart();
-                    }
-                  }
-                }
+              initChart(newSettings);
+            }
+          }, true);
+
+
+          function initChart(newSettings) {
+            firstPointData = {
+              values: [{x:0.5, y:0.6}]
+            };
+            var minX = newSettings.firstCriterion.dataSources[0].pvf.range[0];
+            var maxX = newSettings.firstCriterion.dataSources[0].pvf.range[1];
+            var minY = newSettings.secondCriterion.dataSources[0].pvf.range[0];
+            var maxY = newSettings.secondCriterion.dataSources[0].pvf.range[1];
+            chart = nvd3.models.lineChart()
+              .showLegend(true)
+              .useInteractiveGuideline(true)
+              .color(d3.scale.category10().range())
+              .xDomain([minX, maxX])
+              .yDomain([maxY, minY]);
+            //Axis settings
+            chart.xAxis.axisLabel(newSettings.firstCriterion.title);
+            chart.yAxis.axisLabel(newSettings.secondCriterion.title);
+            chart.xAxis.tickFormat(d3.format('.02f'));
+            chart.yAxis.tickFormat(d3.format('.02f'));
+            data = [{
+              values: [
+                { x: 0.1, y: 0.8 },
+                { x: 0.5, y: 0.5 },
+                { x: 0.7, y: 0.2 },
+              ]
+            }, firstPointData];
+            root.append('rect')
+              .attr('width', '100%')
+              .attr('height', '100%')
+              .attr('fill', 'white');
+
+            root
+              .style('width', '500px')
+              .style('height', '500px')
+              .style('background', 'white')
+              .datum(data)
+              .call(chart);
+
+            var background = root.select('.nv-focus .nv-background rect');
+            background.on('click', clickHandler);
+
+            function clickHandler() {
+
+              var coords = d3.mouse(this);
+
+              var xScale = chart.xAxis.scale();
+              var yScale = chart.yAxis.scale();
+              firstPointData.values[0] = {
+                x: xScale.invert(coords[0]),  // Takes the pixel number to convert to number
+                y: yScale.invert(coords[1])
+              };
+              scope.coordinates = firstPointData.values[0];
+              $timeout(function() {
+                chart.update();
               });
             }
-          });
 
-          function updateChart() {
+          }
+
+          function updateFirstPoint() {
+            firstPointData.values = scope.coordinates;
             $timeout(function() {
-              if ($.isNumeric(scope.coordinates.x) && $.isNumeric(scope.coordinates.y)) {
-                if (scope.coordinates.x > scope.coordinates.xMax) {
-                  scope.coordinates.x = scope.coordinates.xMax;
-                }
-                if (scope.coordinates.x < scope.coordinates.xMin) {
-                  scope.coordinates.x = scope.coordinates.xMin;
-                }
-                if (scope.coordinates.y > scope.coordinates.yMax) {
-                  scope.coordinates.y = scope.coordinates.yMax;
-                }
-                if (scope.coordinates.y < scope.coordinates.yMin) {
-                  scope.coordinates.y = scope.coordinates.yMin;
-                }
-                scope.willingnessChart.data.datasets[0].data = [{
-                  x: scope.coordinates.x,
-                  y: scope.coordinates.y
-                }];
-
-                scope.willingnessChart.update();
-              }
+              chart.update();
             });
           }
+
         }
       };
     };
