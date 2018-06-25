c141c526ed76133bc2860f3da3755173caeea703
Joris de Keijser <chinook25@gmail.com>
2018-06-25 10:06:38 +0200

    wip

--- app/js/preferences/willingnessToTradeOffChartDirective.js
+++ app/js/preferences/willingnessToTradeOffChartDirective.js
@@ -2,26 +2,36 @@
 define(['chartjs', 'jQuery'],
   function(Chart, $) {
     var dependencies = [
-
+      '$timeout',
+      'mcdaRootPath'
     ];
     var WillingnessToTradeOffChartDirective = function(
-
+      $timeout,
+      mcdaRootPath
     ) {
       return {
         restrict: 'E',
         scope: {
           settings: '='
         },
-        template: '<div><canvas id="willingnessChart" width="400" height="400"></canvas></div>',
-        link: function(scope, element) {
-          scope.$watch('settings', function(newSettings){
-            if(newSettings){
+        templateUrl: mcdaRootPath + 'js/preferences/willingnessToTradeOffChartDirective.html',
+        link: function(scope) {
+
+          scope.updateChart = updateChart;
+
+          scope.coordinates = {};
+
+          scope.$watch('settings', function(newSettings) {
+            if (newSettings) {
               var ctx = document.getElementById('willingnessChart').getContext('2d');
-              new Chart(ctx, {
+              scope.willingnessChart = new Chart(ctx, {
                 type: 'scatter',
                 data: {
-                  labels: [],
-                  datasets: []
+                  datasets: [{
+                    label: 'First point',
+                    fill: false,
+                    data: []
+                  }]
                 },
                 options: {
                   scales: {
@@ -37,60 +47,65 @@ define(['chartjs', 'jQuery'],
                         max: scope.settings.firstCriterion.dataSources[0].pvf.range[1]
                       }
                     }]
+                  },
+                  onClick: function(evt) {
+                    // console.log(evt.offsetX + ',' + evt.offsetY);
+                    var yTop = scope.willingnessChart.chartArea.top;
+                    var yBottom = scope.willingnessChart.chartArea.bottom;
+                    scope.coordinates.yMin = scope.willingnessChart.scales['y-axis-1'].min;
+                    scope.coordinates.yMax = scope.willingnessChart.scales['y-axis-1'].max;
+                    var newY = '';
+                    var showstuff = 0;
+                    if (evt.offsetY <= yBottom && evt.offsetY >= yTop) {
+                      newY = Math.abs((evt.offsetY - yTop) / (yBottom - yTop));
+                      newY = (newY - 1) * -1;
+                      newY = newY * (Math.abs(scope.coordinates.yMax - scope.coordinates.yMin)) + scope.coordinates.yMin;
+                      showstuff = 1;
+                    }
+                    var xTop = scope.willingnessChart.chartArea.left;
+                    var xBottom = scope.willingnessChart.chartArea.right;
+                    scope.coordinates.xMin = scope.willingnessChart.scales['x-axis-1'].min;
+                    scope.coordinates.xMax = scope.willingnessChart.scales['x-axis-1'].max;
+                    var newX = '';
+                    if (evt.offsetX <= xBottom && evt.offsetX >= xTop && showstuff === 1) {
+                      newX = Math.abs((evt.offsetX - xTop) / (xBottom - xTop));
+                      newX = newX * (Math.abs(scope.coordinates.xMax - scope.coordinates.xMin)) + scope.coordinates.xMin;
+                    }
+                    if (newY !== '' && newX !== '') {
+                      // console.log(newX + ',' + newY);
+                      scope.coordinates.x = newX;
+                      scope.coordinates.y = newY;
+                      updateChart();
+                    }
                   }
                 }
               });
             }
           });
-          var dim = getParentDimension(element);
 
+          function updateChart() {
+            $timeout(function() {
+              if ($.isNumeric(scope.coordinates.x) && $.isNumeric(scope.coordinates.y)) {
+                if (scope.coordinates.x > scope.coordinates.xMax) {
+                  scope.coordinates.x = scope.coordinates.xMax;
+                }
+                if (scope.coordinates.x < scope.coordinates.xMin) {
+                  scope.coordinates.x = scope.coordinates.xMin;
+                }
+                if (scope.coordinates.y > scope.coordinates.yMax) {
+                  scope.coordinates.y = scope.coordinates.yMax;
+                }
+                if (scope.coordinates.y < scope.coordinates.yMin) {
+                  scope.coordinates.y = scope.coordinates.yMin;
+                }
+                scope.willingnessChart.data.datasets[0].data = [{
+                  x: scope.coordinates.x,
+                  y: scope.coordinates.y
+                }];
 
-          // var svg = d3.select(element[0]).append('svg')
-          //   .attr('width', '100%')
-          //   .attr('height', '100%');
-          // nv.addGraph(function() {
-          //   var data = [
-          //     {
-          //       values: [{ x: 5, y: 5 },{ x: 4, y: 4 },{ x: 3, y: 3 },{ x: 2, y: 2 }, { x: 1, y: 1 }],
-          //       key: 'test',
-          //       color: 'black'
-          //     },
-          //     {
-          //       values: [{ x: 3, y: 3, shape: 'diamond'}],
-          //       key: 'test2 ',
-          //       color: 'red'
-          //     }
-          //   ];
-          //   var chart = nv.models.lineChart()
-          //     .height(dim.height)
-          //     .width(dim.width)
-          //     .useVoronoi(true)
-          //     .interpolate('linear')
-          //     .pointActive(function(dot) {
-          //       return dot.shape === 'diamond';
-          //     })
-          //     .duration(300)
-          //     .color(d3.scale.category10().range());
-          //   chart.xAxis.tickFormat(d3.format(',.3g'));
-          //   chart.yAxis.tickFormat(d3.format(',.3g'));
-          //   svg.datum(data).call(chart);
-          //   nv.utils.windowResize(chart.update);
-          // });
-          // svg.selectAll('svg .nv-lineChart .nv-point').style('stroke-width',
-          // '7px').style('fill-opacity', '.95').style('stroke-opacity', '.95');
-
-
-          function getParentDimension(element) {
-            var width = parsePx($(element[0].parentNode).css('width'));
-            var height = parsePx($(element[0].parentNode).css('height'));
-
-            return {
-              width: width,
-              height: height
-            };
-          }
-          function parsePx(str) {
-            return parseInt(str.replace(/px/gi, ''));
+                scope.willingnessChart.update();
+              }
+            });
           }
         }
       };
