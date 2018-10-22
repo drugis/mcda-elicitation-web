c96d5bbcf532c9a1a4345ea10885da0e5cd073c5
Joris de Keijser <chinook25@gmail.com>
2018-10-22 15:22:22 +0200

    add tests

--- app/js/benefitRisk/mcdaBenefitRiskController.js
+++ app/js/benefitRisk/mcdaBenefitRiskController.js
@@ -32,19 +32,21 @@ define(['lodash'], function(_) {
     $scope.newScenario = newScenario;
     $scope.scenarioChanged = scenarioChanged;
 
+    // set on scope for testing purposes
+    $scope.updateScales = updateScales;
+    $scope.updateState = updateState;
+    $scope.deregisterTransitionListener = $transitions.onStart({}, function(transition) {
+      setActiveTab(transition.to().name, transition.to().name);
+    });
+
     // init
     var baseProblem = $scope.workspace.problem;
-    var deregisterTransitionListener;
     $scope.isEditTitleVisible = false;
     $scope.scenarioTitle = {};
     $scope.selections = {};
     $scope.isDuplicateScenarioTitle = false;
     $scope.scalesPromise = WorkspaceService.getObservedScales(baseProblem);
     $scope.tasks = _.keyBy(Tasks.available, 'id');
-
-    // set on scope for testing purposes
-    $scope.updateScales = updateScales;
-    $scope.updateState = updateState;
 
     $scope.updateState(currentScenario);
     $scope.effectsTableInfo = EffectsTableService.createEffectsTableInfo(baseProblem.performanceTable);
@@ -55,15 +57,12 @@ define(['lodash'], function(_) {
     $scope.isStandalone = isMcdaStandalone;
     determineActiveTab();
 
-    deregisterTransitionListener = $transitions.onStart({}, function(transition) {
-      setActiveTab(transition.to().name, transition.to().name);
-    });
-
     $scope.$watch('scenario.state', updateTaskAccessibility);
     $scope.$watch('aggregateState', checkHasNoStochasticResults, true);
-    $scope.$on('$destroy', deregisterTransitionListener);
+    $scope.$on('$destroy', function() {
+      $scope.deregisterTransitionListener();
+    });
     $scope.$on('elicit.settingsChanged', function() {
-      console.log('triggered');
       $scope.updateScales($scope.workspace.scales.base);
     });
     $scope.$on('elicit.resultsAccessible', function(event, scenario) {
@@ -86,10 +85,8 @@ define(['lodash'], function(_) {
         $scope.updateScales(observedScales);
       });
 
-      updateTaskAccessibility();
       updateScenarios();
     }
-
 
     function checkHasNoStochasticResults() {
       $scope.hasNoStochasticResults = WorkspaceService.hasNoStochasticResults($scope.aggregateState);
@@ -120,12 +117,6 @@ define(['lodash'], function(_) {
       });
     }
 
-    function findAvailableTask(taskId) {
-      return _.find(Tasks.available, function(task) {
-        return task.id === taskId;
-      });
-    }
-
     function determineActiveTab() {
       setActiveTab($state.current.name, 'evidence');
     }
@@ -135,13 +126,18 @@ define(['lodash'], function(_) {
       $scope.activeTab = task ? task.activeTab : defaultStateName;
     }
 
+    function findAvailableTask(taskId) {
+      return _.find(Tasks.available, function(task) {
+        return task.id === taskId;
+      });
+    }
+
     function updateTaskAccessibility() {
       $scope.tasksAccessibility = {
         preferences: TaskDependencies.isAccessible($scope.tasks.preferences, $scope.aggregateState).accessible,
         results: TaskDependencies.isAccessible($scope.tasks.results, $scope.aggregateState).accessible
       };
     }
-
 
     function forkScenario() {
       $modal.open({
--- frontend-test/unit/benefitRiskControllerSpec.js
+++ frontend-test/unit/benefitRiskControllerSpec.js
@@ -76,11 +76,11 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
         $promise: $q.resolve(scenarios)
       });
       EffectsTableService.createEffectsTableInfo.and.returnValue(effectsTableInfo);
+      transitions.onStart.and.returnValue(() => { });
     }));
 
     beforeEach(inject(($controller, $rootScope) => {
       scope = $rootScope.$new();
-
       scope.workspace = workspace;
 
       $controller('MCDABenefitRiskController', {
@@ -110,27 +110,37 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
         TaskDependencies.isAccessible.calls.reset();
         scope.$apply();
       });
+
       it('should place a forkScenario function which opens a modal on the scope.', () => {
         expect(modalMock.open).not.toHaveBeenCalled();
         scope.forkScenario();
         expect(modalMock.open).toHaveBeenCalled();
       });
+
       it('should place a newScenario function which opens a modal on the scope.', () => {
         expect(modalMock.open).not.toHaveBeenCalled();
         scope.newScenario();
         expect(modalMock.open).toHaveBeenCalled();
       });
+
+      it('should create a deregister transition listener', () => {
+        expect(transitions.onStart).toHaveBeenCalled();
+      });
+
       describe('should place scenarioChanged on the scope', () => {
         beforeEach(() => {
           stateMock.go.calls.reset();
         });
+
         it('which should be a function', () => {
           expect(typeof scope.scenarioChanged).toBe('function');
         });
+
         it('which should do nothing if there is no new scenario', () => {
           scope.scenarioChanged();
           expect(stateMock.go).not.toHaveBeenCalled();
         });
+
         it('which should go to the correct state for smaa-results', () => {
           var oldStateCurrent = stateMock.current;
           stateMock.current = {
@@ -147,6 +157,7 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
           });
           stateMock.current = oldStateCurrent;
         });
+
         it('which should go to preferences by default', () => {
           var oldStateCurrent = stateMock.current;
           stateMock.current = {
@@ -164,12 +175,14 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
           stateMock.current = oldStateCurrent;
         });
       });
+
       it('should populate the scope with scenarios', () => {
         expect(scope.scenarios).toBe(scenarios);
         expect(scope.scenario).toBe(currentScenario);
         expect(scope.scenariosWithResults).toBe(scenariosWithResults);
         expect(scope.isDuplicateScenarioTitle).toBe(false);
       });
+
       it('should place the tasks on the scope', () => {
         expect(scope.tasks).toEqual({
           'smaa-results': {
@@ -180,12 +193,15 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
           }
         });
       });
+
       it('should retrieve the observed scales', () => {
         expect(WorkspaceService.getObservedScales).toHaveBeenCalledWith(workspace.problem);
       });
+
       it('should build a base aggregate state and place it on the scope', () => {
         expect(scope.baseAggregateState).toBe(baseAggregateState);
       });
+
       it('should set the task accessibility', () => {
         expect(TaskDependencies.isAccessible).toHaveBeenCalledTimes(2);
         expect(scope.tasksAccessibility).toEqual({
@@ -193,13 +209,16 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
           results: true
         });
       });
+
       it('should set the effects table info on the scope', () => {
         expect(scope.effectsTableInfo).toBe(effectsTableInfo);
       });
+
       it('should check for missing performance values', () => {
         expect(scope.hasMissingValues).toBeTruthy();
       });
     });
+
     describe('once the scales have been loaded', () => {
       var observedScales = {
         id: 'observedScales'
@@ -209,9 +228,11 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
       var baseProblemWithScales = _.extend({}, baseAggregateState.problem, {
         criteria: percentifiedCriteria
       });
+
       beforeEach(() => {
         observedScalesDefer.resolve(observedScales);
       });
+
       describe('if percentages should be used', () => {
         var stateWithPercentifiedCriteria = _.merge({}, baseAggregateState, {
           problem: {
@@ -227,6 +248,7 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
           WorkspaceService.setDefaultObservedScales.and.returnValue(baseProblemWithScales);
           scope.$apply();
         });
+
         it('should percentify the scales and properly initalise base- and regular aggregate state', (done) => {
           observedScalesDefer.promise.then(() => {
             expect(scope.workspace.scales.base).toBe(observedScales);
@@ -238,6 +260,7 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
           });
           scope.$apply();
         });
+
         it('should update the task accessibility', () => {
           expect(TaskDependencies.isAccessible).toHaveBeenCalledTimes(4);
           expect(scope.tasksAccessibility).toEqual({
@@ -248,6 +271,7 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
       });
       describe('if percentages should not be used', () => {
         var problemWithBaseCriteria = baseAggregateState;
+
         beforeEach(() => {
           TaskDependencies.isAccessible.calls.reset();
           WorkspaceSettingsService.usePercentage.and.returnValue(false);
@@ -255,6 +279,7 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
           WorkspaceService.setDefaultObservedScales.and.returnValue(problemWithBaseCriteria.problem);
           scope.$apply();
         });
+
         it('should set the scales and properly initalise base- and regular aggregate state', (done) => {
           observedScalesDefer.promise.then(() => {
             expect(scope.workspace.scales.base).toBe(observedScales);
@@ -266,6 +291,7 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
           });
           scope.$apply();
         });
+
         it('should update the task accessibility', () => {
           expect(TaskDependencies.isAccessible).toHaveBeenCalledTimes(4);
           expect(scope.tasksAccessibility).toEqual({
@@ -275,6 +301,7 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
         });
       });
     });
+
     describe('when the aggregate state changes', () => {
       beforeEach(() => {
         WorkspaceService.hasNoStochasticResults.calls.reset();
@@ -282,11 +309,13 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
         scope.aggregateState = { id: 'changed' };
         scope.$apply();
       });
+
       it('should check whether there are stochastic results', () => {
         expect(WorkspaceService.hasNoStochasticResults).toHaveBeenCalled();
         expect(scope.hasNoStochasticResults).toBe(true);
       });
     });
+
     describe('when a settings change event triggers', () => {
       it('should update the scales', () => {
         spyOn(scope, 'updateScales');
@@ -296,6 +325,7 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
         expect(scope.updateScales).toHaveBeenCalled();
       });
     });
+
     describe('when a resultsAccessible event triggers', () => {
       it('should update the state', () => {
         spyOn(scope, 'updateState');
@@ -303,6 +333,16 @@ define(['lodash', 'angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], (
         scope.$broadcast('elicit.resultsAccessible');
         scope.$apply();
         expect(scope.updateState).toHaveBeenCalled();
+      });
+    });
+
+    describe('when a $destroy event triggers', () => {
+      it('should call deregisterTransitionListener', () => {
+        spyOn(scope, 'deregisterTransitionListener');
+        expect(scope.deregisterTransitionListener).not.toHaveBeenCalled();
+        scope.$broadcast('$destroy');
+        scope.$apply();
+        expect(scope.deregisterTransitionListener).toHaveBeenCalled();
       });
     });
   });
