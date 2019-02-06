e31f8d638d04855c296096f2af5da3e58ad6f3a2
Joris de Keijser <chinook25@gmail.com>
2019-02-06 13:25:20 +0100

    harmonisation fix

--- app/js/effectsTable/effectsTableCellDirective.js
+++ app/js/effectsTable/effectsTableCellDirective.js
@@ -15,11 +15,11 @@ define([], function() {
       },
       templateUrl: './effectsTableCellDirective.html',
       link: function(scope) {
-        scope.uncertainty = scope.effectsTableInfo.studyDataLabelsAndUncertainty[scope.alternativeId].hasUncertainty || scope.effectsTableInfo.distributionType === 'relative';
+        scope.uncertainty = scope.effectsTableInfo.distributionType === 'relative' || scope.effectsTableInfo.studyDataLabelsAndUncertainty[scope.alternativeId].hasUncertainty;
         scope.effectsDisplay = scope.workspaceSettings.effectsDisplay;
         scope.hasStudyData = scope.effectsTableInfo.hasStudyData;
         scope.distributionType = scope.effectsTableInfo.distributionType;
-        scope.effectDataSourceLabel = scope.effectsTableInfo.studyDataLabelsAndUncertainty[scope.alternativeId].label;
+        scope.effectDataSourceLabel = scope.effectsTableInfo.studyDataLabelsAndUncertainty ? scope.effectsTableInfo.studyDataLabelsAndUncertainty[scope.alternativeId].label : undefined;
       }
 
     };
