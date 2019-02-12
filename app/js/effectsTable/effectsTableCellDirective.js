55621f83b9e76f9a91c646fe2bd9893be684b983
Joris de Keijser <chinook25@gmail.com>
2019-02-12 14:04:42 +0100

    fix effectsview

--- app/js/effectsTable/effectsTableCellDirective.js
+++ app/js/effectsTable/effectsTableCellDirective.js
@@ -18,7 +18,8 @@ define([], function() {
         scope.uncertainty = scope.effectsTableInfo.distributionType === 'relative' || scope.effectsTableInfo.studyDataLabelsAndUncertainty[scope.alternativeId].hasUncertainty;
         scope.effectsDisplay = scope.workspaceSettings.effectsDisplay;
         scope.hasStudyData = scope.effectsTableInfo.hasStudyData;
-        scope.distributionType = scope.effectsTableInfo.distributionType;
+        scope.isEffect = scope.effectsTableInfo.distributionType === 'exact' && !scope.uncertainty;
+        
         scope.effectDataSourceLabel = scope.effectsTableInfo.studyDataLabelsAndUncertainty ? scope.effectsTableInfo.studyDataLabelsAndUncertainty[scope.alternativeId].label : undefined;
       }
 
