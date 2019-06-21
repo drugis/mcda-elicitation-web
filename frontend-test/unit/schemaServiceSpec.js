080f8acea0facddf301cef71dfb876e1182f0477
Joris de Keijser <chinook25@gmail.com>
2019-06-21 12:32:59 +0200

    remove null and undefined properties from data sources

--- frontend-test/unit/schemaServiceSpec.js
+++ frontend-test/unit/schemaServiceSpec.js
@@ -4,7 +4,7 @@ define(['angular', 'angular-mocks', 'mcda/benefitRisk/benefitRisk'], function(an
   var generateUuidMock = function() {
     return 'uuid';
   };
-  var currentSchemaVersion = '1.3.1';
+  var currentSchemaVersion = '1.3.2';
   var schemaService;
 
   describe('The SchemaService', function() {
