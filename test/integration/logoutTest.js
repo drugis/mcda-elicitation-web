379c869ec1b47b82914fca6c46a6963ac2696b41
karlis <karlis.zalite@gmail.com>
2019-10-07 09:43:51 +0200

    logout test ok on firefox

--- test/integration/logoutTest.js
+++ test/integration/logoutTest.js
@@ -5,14 +5,15 @@ const loginService = require('./util/loginService.js');
 const testUrl = 'http://localhost:3002';
 
 module.exports = {
-  'MCDA logout' : function (browser) {
-    loginService.login(browser, testUrl, loginService.username, loginService.correctPassword)
-      // .pause(1000)
-      // .moveToElement('#userImageLink', 0, 0)
-      // .moveToElement('#logoutLink', 0, 0)
-      // .click('#logoutLink')
-      // .pause(1000)
-      // .waitForElementVisible('#signinButton')
+  'MCDA logout': function(browser) {
+    loginService.login(browser, testUrl, loginService.username, loginService.correctPassword);
+
+    browser
+      .waitForElementVisible('#workspaces-header')
+      .moveToElement('#user-image-link', 0, 0)
+      .moveToElement('#logout-link', 0, 0)
+      .click('#logout-link')
+      .waitForElementVisible('#signinButton')
       .end();
-  },  
+  },
 };
