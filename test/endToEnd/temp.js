c5892430d7b455866027ce112480b6844c58f87c
karlis <karlis.zalite@gmail.com>
2019-12-03 09:17:08 +0100

    test

---
+++ test/endToEnd/temp.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const loginService = require('./util/loginService.js');
+const workspaceService = require('./util/workspaceService.js');
+const testUrl = require('./util/constants').testUrl;
+
+module.exports = {
+  'test': function(browser) {
+    const title = 'Antidepressants - single study B/R analysis (Tervonen et al, Stat Med, 2011)';
+
+    loginService.login(browser, testUrl, loginService.username, loginService.correctPassword);
+    // workspaceService.addExample(browser, title);
+    // workspaceService.deleteFromList(browser, 0);
+    browser.end();
+  },
+};
