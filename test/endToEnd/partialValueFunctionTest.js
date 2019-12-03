27c426eefe45cbe4a508a8ebae67b6f558c5bd0b
karlis <karlis.zalite@gmail.com>
2019-12-03 15:04:25 +0100

    test

---
+++ test/endToEnd/partialValueFunctionTest.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const loginService = require('./util/loginService');
+const workspaceService = require('./util/workspaceService');
+const errorService = require('./util/errorService');
+
+const testUrl = require('./util/constants').testUrl;
+const workspacePath = '/partialValueFunctionTestProblem.json';
+
+module.exports = {
+  beforeEach: function(browser) {
+    loginService.login(browser, testUrl, loginService.username, loginService.correctPassword);
+    workspaceService.uploadTestWorkspace(browser, workspacePath);
+    browser
+      .waitForElementVisible('#workspace-title')
+      .click('#preferences-tab')
+      .waitForElementVisible('#partial-value-functions-block');
+  },
+
+  afterEach: function(browser) {
+    browser.click('#logo');
+    workspaceService.deleteFromList(browser, 0);
+    errorService.isErrorBarHidden(browser);
+    browser.end();
+
+  },
+
+  'Set partial value functions': function(browser) {
+    browser
+      .click('#c1-pvf-button')
+      .click('#save-button')
+      .click('#c2-pvf-button')
+      .click('#increasing-pvf-option')
+      .click('#piece-wise-pvf-option')
+      .click('#next-button')
+      .click('#piece-wise-next-button')
+      .click('#piece-wise-next-button')
+      .click('#piece-wise-save-button')
+      .waitForElementVisible('#ranking-button:enabled');
+  },
+
+  'Go to previous step': function(browser) {
+    browser
+      .click('#c1-pvf-button')
+      .click('#piece-wise-pvf-option')
+      .click('#next-button')
+      .click('#previous-button')
+      .waitForElementVisible('#criterion-title');
+  }
+};
