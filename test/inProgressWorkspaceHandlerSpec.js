0aa7ba8f8993e26403111cdd4f4ef6501bd54982
karlis <karlis.zalite@gmail.com>
2019-12-04 09:36:23 +0100

    Test

--- test/inProgressWorkspaceHandlerSpec.js
+++ test/inProgressWorkspaceHandlerSpec.js
@@ -52,7 +52,6 @@ describe('the in progress workspace handler', () => {
     const next = chai.spy();
 
     it('should call the in progress workspace repository with the correct arguments', () => {
-      fail();
       const createdId = 321;
       const result = {
         rows: [{
