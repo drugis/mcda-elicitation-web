7f4997ef72f93354ef1e0e9578cf2c2349dbca1e
chinook25 <chinook25@gmail.com>
2019-12-06 09:49:14 +0100

    show weights in preferences tab (#120)

--- app/js/preferences/preferencesService.js
+++ app/js/preferences/preferencesService.js
@@ -1,40 +1,65 @@
 'use strict';
-define(['lodash', 'angular'], function(_) {
+define(['lodash', 'angular'], function(_, angular) {
 
-  var dependencies = [];
-  var PreferencesService = function() {
+  var dependencies = [
+    'PataviResultsService'
+  ];
+  var PreferencesService = function(
+    PataviResultsService
+  ) {
 
     function buildImportance(criteria, preferences) {
       return _.reduce(criteria, function(accum, criterion) {
-        var preference;
         if (_.isEmpty(preferences)) {
-          // values are unknown
           accum[criterion.id] = '?';
-          return accum;
-        }
-        if (preferences[0].type === 'ordinal') {
-          // determine rank for each criterion
-          preference = _.findIndex(preferences, function(pref) {
-            return pref.criteria[1] === criterion.id;
-          });
-          accum[criterion.id] = preference !== undefined ? preference + 2 : 1;
+        } else if (preferences[0].type === 'ordinal') {
+          accum[criterion.id] = determineRank(preferences, criterion.id);
         } else {
-          preference = _.find(preferences, function(pref) {
-            return pref.criteria[1] === criterion.id;
-          });
-          if (!preference) {
-            accum[criterion.id] = '100%';
-          } else if (preference.type === 'exact swing') {
-            accum[criterion.id] = Math.round((1 / preference.ratio) * 100) + '%';
-          } else if (preference.type === 'ratio bound') {
-            accum[criterion.id] = Math.round((1 / preference.bounds[1]) * 100) + '-' + Math.round((1 / preference.bounds[0]) * 100) + '%';
-          }
+          accum[criterion.id] = buildCriterionImportance(preferences, criterion.id);
         }
         return accum;
       }, {});
     }
+
+    function determineRank(preferences, criterionId) {
+      var preference = _.findIndex(preferences, function(pref) {
+        return pref.criteria[1] === criterionId;
+      });
+      return preference !== undefined ? preference + 2 : 1;
+    }
+
+    function buildCriterionImportance(preferences, criterionId) {
+      var preference = _.find(preferences, function(pref) {
+        return pref.criteria[1] === criterionId;
+      });
+      if (!preference) {
+        return '100%';
+      } else if (preference.type === 'exact swing') {
+        return getExactValue(preference);
+      } else if (preference.type === 'ratio bound') {
+        return getImpreciseValue(preference);
+      }
+    }
+
+    function getImpreciseValue(preference) {
+      return Math.round((1 / preference.bounds[1]) * 100) + '-' +
+        Math.round((1 / preference.bounds[0]) * 100) + '%';
+    }
+
+    function getExactValue(preference) {
+      return Math.round((1 / preference.ratio) * 100) + '%';
+    }
+
+    function getWeights(problem) {
+      var newProblem = angular.copy(problem);
+      newProblem.method = 'representativeWeights';
+      console.log(PataviResultsService)
+      return PataviResultsService.postAndHandleResults(newProblem);
+    }
+
     return {
-      buildImportance: buildImportance
+      buildImportance: buildImportance,
+      getWeights: getWeights
     };
   };
   return dependencies.concat(PreferencesService);
