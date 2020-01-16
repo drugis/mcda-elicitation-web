d905d7a2b5f522e8a2af492b5c4c968af42edf59
karlis <karlis.zalite@gmail.com>
2020-01-16 15:09:39 +0100

    add tests

--- test/endToEnd/logoutTest.js
+++ test/endToEnd/logoutTest.js
@@ -1,15 +1,54 @@
 'use strict';
 
-const loginService = require('./util/loginService.js');
+const loginService = require('./util/loginService');
+const constants = require('./util/constants');
 
 module.exports = {
-  'MCDA logout': function(browser) {
-    loginService.login(browser)
-      .waitForElementVisible('#workspaces-header')
-      .moveToElement('#user-image-link', 0, 0)
-      .moveToElement('#logout-link', 0, 0)
-      .click('#logout-link')
-      .waitForElementVisible('#signinButton')
-      .end();
-  },
+  beforeEach: beforeEach,
+  afterEach: afterEach,
+  'Logout via user name': logoutViaName,
+  'Logout via URL': logoutViaURL,
+  'Logout and login with the same user': sameUserLogoutLogin,
+  'Logout and login with different users': logoutLoginOtherUser
 };
+
+function beforeEach(browser) {
+  loginService.login(browser)
+    .waitForElementVisible('#workspaces-header');
+}
+
+function afterEach(browser) {
+  browser
+    .waitForElementVisible('#signinButton')
+    .end();
+}
+
+function logoutViaName(browser) {
+  browser
+    .moveToElement('#user-image-link', 0, 0)
+    .moveToElement('#logout-link', 0, 0)
+    .click('#logout-link');
+}
+
+function logoutViaURL(browser) {
+  browser.url(constants.TEST_URL + '/logout');
+}
+
+function sameUserLogoutLogin(browser) {
+  browser
+    .url(constants.TEST_URL + '/logout')
+    .waitForElementVisible('#signinButton');
+  loginService.login(browser)
+    .waitForElementVisible('#workspaces-header');
+  browser.url(constants.TEST_URL + '/logout');
+}
+
+function logoutLoginOtherUser(browser) {
+  browser
+    .url(constants.TEST_URL + '/logout')
+    .waitForElementVisible('#signinButton');
+
+  loginService.login(browser, 'user2')
+    .waitForElementVisible('#workspaces-header')
+    .url(constants.TEST_URL + '/logout');
+}
