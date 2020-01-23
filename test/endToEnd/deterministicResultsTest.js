8702fce7303fa5b16e0b88e2c52ad68ccd22d5eb
Karlis Zalite <karlis.zalite@gmail.com>
2020-01-23 11:20:59 +0100

    add value profile table

--- test/endToEnd/deterministicResultsTest.js
+++ test/endToEnd/deterministicResultsTest.js
@@ -1,8 +1,16 @@
 'use strict';
 
+module.exports = {
+  beforeEach: beforeEach,
+  afterEach: afterEach,
+  'Deterministic results': results,
+  'Deterministic results with recalculated values': recalculatedResults,
+  'Switch alternative and criterion for one-way sensitivity analysis measurements plot': modifyMeasurementsPlot,
+  'Switch criterion for one-way sensitivity analysis preferences plot': modifyPreferencesPlot
+};
+
 const loginService = require('./util/loginService');
 const workspaceService = require('./util/workspaceService');
-const errorService = require('./util/errorService');
 
 const chai = require('chai');
 
@@ -10,7 +18,7 @@ function checkElementValueGreaterThan(browser, path, value) {
   browser
     .useXpath()
     .getLocationInView(path)
-    .getText(path, function(result) {
+    .getText(path, function (result) {
       chai.expect(parseFloat(result.value)).to.be.above(value);
     })
     .useCss();
@@ -19,7 +27,7 @@ function checkElementValueGreaterThan(browser, path, value) {
 function checkResetMeasurementValue(browser, path) {
   browser
     .useXpath()
-    .getText(path, function(result) {
+    .getText(path, function (result) {
       chai.expect(parseFloat(result.value)).to.be.below(60);
       chai.expect(parseFloat(result.value)).to.be.above(36);
     })
@@ -41,8 +49,9 @@ function beforeEach(browser) {
 
 function afterEach(browser) {
   browser.click('#logo');
-  workspaceService.deleteFromList(browser, 0);
-  errorService.isErrorBarHidden(browser).end();
+  workspaceService
+    .deleteFromList(browser, 0)
+    .end();
 }
 
 function results(browser) {
@@ -51,15 +60,18 @@ function results(browser) {
     .waitForElementVisible('#base-case-table')
     .waitForElementVisible('#base-case-plot')
     .waitForElementVisible('#measurements-sensitivity-plot')
-    .waitForElementVisible('#preferences-sensitivity-plot');
+    .waitForElementVisible('#preferences-sensitivity-plot')
+    .waitForElementVisible('#base-value-profile-table');
 
   const measurementValuePath = '//sensitivity-table//tr[2]/td[4]//span[1]';
   const weightValuePath = '//*[@id="criterion-0-weight"]';
   const baseCaseValuePath = '//*[@id="alternative-0-base-case"]';
+  const baseCaseValueTablePath = '//value-profile-table/table/tbody/tr[1]/td[2]';
 
   checkElementValueGreaterThan(browser, measurementValuePath, 30);
   checkElementValueGreaterThan(browser, weightValuePath, 0.2);
   checkElementValueGreaterThan(browser, baseCaseValuePath, 0.7);
+  checkElementValueGreaterThan(browser, baseCaseValueTablePath, 0.01);
 }
 
 function recalculatedResults(browser) {
@@ -77,6 +89,7 @@ function recalculatedResults(browser) {
     .assert.containsText(measurementValuePath, '63 (36.')
     .waitForElementVisible('//*[@id="recalculated-case-table"]')
     .waitForElementVisible('//*[@id="recalculated-case-plot"]')
+    .waitForElementVisible('//*[@id="recalculated-value-profile-table"]')
     .useCss();
 
   const recalculatedCaseValuePath = '//*[@id="alternative-0-recalculated-case"]';
@@ -87,8 +100,9 @@ function recalculatedResults(browser) {
   checkResetMeasurementValue(browser, measurementValuePath);
 
   browser
-.assert.not.elementPresent('#recalculated-case-table')
-.assert.not.elementPresent('#recalculated-case-plot');
+    .assert.not.elementPresent('#recalculated-case-table')
+    .assert.not.elementPresent('#recalculated-case-plot')
+    .assert.not.elementPresent('#recalculated-value-profile-table');
 }
 
 function modifyMeasurementsPlot(browser) {
@@ -108,12 +122,3 @@ function modifyPreferencesPlot(browser) {
     .click('option[label="Nausea\ ADRs"]')
     .assert.containsText('#preferences-criterion-selector', 'Nausea\ ADRs');
 }
-
-module.exports = {
-  beforeEach: beforeEach,
-  afterEach: afterEach,
-  'Deterministic results': results,
-  'Deterministic results with recalculated values': recalculatedResults,
-  'Switch alternative and criterion for one-way sensitivity analysis measurements plot': modifyMeasurementsPlot,
-  'Switch criterion for one-way sensitivity analysis preferences plot': modifyPreferencesPlot
-};
