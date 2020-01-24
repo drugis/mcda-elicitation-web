83263f2780ca85d43e30aa3a960ddd8aad593ef6
chinook25 <chinook25@gmail.com>
2020-01-24 13:17:44 +0100

    add spinners, refactoring matching in doing so (#137)
    * add spinners, refactoring matching in doing so

--- app/js/preferences/elicitationTradeOffDirective.js
+++ app/js/preferences/elicitationTradeOffDirective.js
@@ -1,15 +1,11 @@
 'use strict';
-define(['lodash', 'd3', 'c3'],
-  function(_, d3, c3) {
+define([],
+  function() {
     var dependencies = [
-      '$timeout',
-      'PartialValueFunctionService',
       'TradeOffService',
       'significantDigits'
     ];
-    var PreferenceElicitationRow = function(
-      $timeout,
-      PartialValueFunctionService,
+    var ElicitationTradeOffDirective = function(
       TradeOffService,
       significantDigits
     ) {
@@ -21,29 +17,21 @@ define(['lodash', 'd3', 'c3'],
           weight: '='
         },
         templateUrl: './elicitationTradeOffDirective.html',
-        link: function(scope, element) {
-          var root, data, chart;
+        link: function(scope) {
           var minX = scope.secondaryCriterion.dataSources[0].pvf.range[0];
           var maxX = scope.secondaryCriterion.dataSources[0].pvf.range[1];
           var minY = scope.mostImportantCriterion.dataSources[0].pvf.range[0];
           var maxY = scope.mostImportantCriterion.dataSources[0].pvf.range[1];
-          scope.pvf = PartialValueFunctionService;
 
-          scope.plotIndifference = plotIndifference;
-
-          $timeout(init);
+          init();
 
           function init() {
-            root = d3.select(element[0]);
-            root = root.select('#tradeOffPlot');
-            data = {
-              xs: {
-                line: 'line_x'
-              },
-              columns: [],
-              type: 'line',
-            };
+            setCriterionValues();
+            setScopeValues();
+            getPlotValues();
+          }
 
+          function setCriterionValues() {
             if (scope.mostImportantCriterion.hasOwnProperty('isFavorable') &&
               !scope.mostImportantCriterion.isFavorable
             ) {
@@ -51,18 +39,6 @@ define(['lodash', 'd3', 'c3'],
             } else {
               setDefaultValues();
             }
-
-            scope.sliderOptions = {
-              precision: 4,
-              onEnd: plotIndifference,
-              translate: significantDigits,
-              floor: significantDigits(minY),
-              ceil: significantDigits(maxY)
-            };
-            scope.sliderOptions.step = significantDigits((maxY - minY) / 50);
-
-            initChart();
-            plotIndifference();
           }
 
           function setUnfavorableValues() {
@@ -76,7 +52,6 @@ define(['lodash', 'd3', 'c3'],
             };
             scope.question = 'How much worse is ' + scope.mostImportantCriterion.title +
               ' maximally allowed to get to justify the improvement of ' + scope.secondaryCriterion.title + '?';
-
           }
 
           function setDefaultValues() {
@@ -92,60 +67,54 @@ define(['lodash', 'd3', 'c3'],
               ' minimally become to justify the worsening of ' + scope.secondaryCriterion.title + '?';
           }
 
-          function initChart() {
+          function setScopeValues() {
+            scope.sliderOptions = {
+              precision: 3,
+              onEnd: getPlotValues,
+              translate: significantDigits,
+              floor: significantDigits(minY),
+              ceil: significantDigits(maxY),
+              step: significantDigits((maxY - minY) / 50)
+            };
             scope.areCoordinatesSet = false;
             scope.inputCoordinates = {};
-            root.append('rect')
-              .attr('width', '100%')
-              .attr('height', '100%')
-              .attr('fill', 'white');
-            root
-              .style('width', '300px')
-              .style('height', '300px')
-              .style('background', 'white');
-
-            var coordRanges = {
+            scope.coordRanges = {
               minX: minX,
               maxX: maxX,
               minY: minY,
               maxY: maxY
             };
-
-            var criteria = {
+            scope.criteria = {
               firstCriterion: scope.secondaryCriterion,
               secondCriterion: scope.mostImportantCriterion
             };
-
-            var initialSettings = TradeOffService.getInitialSettings(root, data, coordRanges, criteria);
-            initialSettings.data.columns = [];
-            initialSettings.legend.show = false;
-            initialSettings.axis.x.tick.count = 5;
-            initialSettings.axis.y.tick.count = 5;
-
-            scope.units = {
-              x: scope.secondaryCriterion.dataSources[0].unitOfMeasurement.label,
-              y: scope.mostImportantCriterion.dataSources[0].unitOfMeasurement.label
-            };
-
-            chart = c3.generate(initialSettings);
           }
 
-          function plotIndifference() {
+          function getPlotValues() {
             scope.showSlider = false;
+            delete scope.weight.value;
             TradeOffService.getElicitationTradeOffCurve(
               scope.mostImportantCriterion,
               scope.secondaryCriterion,
               scope.mostImportantCriterionValue.secondValue
             ).then(function(results) {
-              data.columns[0] = ['line_x'].concat(results.data.x);
-              data.columns[1] = ['line'].concat(results.data.y);
-              chart.load(data);
+              scope.data = createData(results);
               scope.weight.value = results.weight;
             });
           }
 
+          function createData(results) {
+            return {
+              xs: { line: 'line_x' },
+              columns: [
+                ['line_x'].concat(results.data.x),
+                ['line'].concat(results.data.y)
+              ],
+              type: 'line',
+            };
+          }
         }
       };
     };
-    return dependencies.concat(PreferenceElicitationRow);
+    return dependencies.concat(ElicitationTradeOffDirective);
   });
