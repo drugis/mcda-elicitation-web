d2a845efa6843b5f2cda69caa9aa807e9b4b9ed7
Karlis Zalite <karlis.zalite@gmail.com>
2020-06-25 13:31:59 +0200

    navigate to manual input from workspaces; display "empty title"

--- app/js/workspace/inProgressWorkspacesDirective.js
+++ app/js/workspace/inProgressWorkspacesDirective.js
@@ -1,38 +1,48 @@
 'use strict';
-define(['lodash'], function(_) {
-  var dependencies = [
-    '$modal'
-  ];
-  var InProgressWorkspacesDirective = function(
-    $modal
-  ) {
+define(['lodash'], function (_) {
+  var dependencies = ['$modal'];
+  var InProgressWorkspacesDirective = function ($modal) {
     return {
       restrict: 'E',
       scope: {
-        'inProgressWorkspaces': '='
+        inProgressWorkspaces: '='
       },
       templateUrl: './inProgressWorkspacesDirective.html',
-      link: function(scope) {
+      link: function (scope) {
         scope.deleteInProgress = deleteInProgress;
+        scope.getTitle = getTitle;
 
         function deleteInProgress(id, title) {
           $modal.open({
             templateUrl: './deleteWorkspace.html',
             controller: 'DeleteInProgressController',
             resolve: {
-              callback: function() {
-                return function() {
-                  scope.inProgressWorkspaces = _.reject(scope.inProgressWorkspaces, ['id', id]);
+              callback: function () {
+                return function () {
+                  scope.inProgressWorkspaces = _.reject(
+                    scope.inProgressWorkspaces,
+                    ['id', id]
+                  );
                 };
               },
-              inProgressId: function() {
+              inProgressId: function () {
                 return id;
               },
-              title: function() {
+              title: function () {
                 return title;
               }
             }
           });
+        }
+
+        function getTitle(inProgressWorkspace) {
+          if (inProgressWorkspace.state.title) {
+            return inProgressWorkspace.state.title;
+          } else if (inProgressWorkspace.title) {
+            return inProgressWorkspace.title;
+          } else {
+            return 'Empty title';
+          }
         }
       }
     };
