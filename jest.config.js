4626002ac2326bd186f0d6e048d139748d0ec0e7
Karlis Zalite <karlis.zalite@gmail.com>
2020-06-30 14:29:00 +0200

    add test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // The root of your source code, typically /src
   // `<rootDir>` is a token Jest substitutes
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/test', '<rootDir>/app/ts'],
 
   // Jest transformations -- this adds support for TypeScript
   // using ts-jest
