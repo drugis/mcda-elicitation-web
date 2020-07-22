67d833c834fa52aff0e09542337dd419c4e34244
Karlis Zalite <karlis.zalite@gmail.com>
2020-07-22 13:29:10 +0200

    test a fix for failing test

--- test/endToEnd/randomSeedTest.js
+++ test/endToEnd/randomSeedTest.js
@@ -49,6 +49,7 @@ function set(browser) {
 function setInvalid(browser) {
   browser
     .setValue('#random-seed', 1.5)
+    .pause(500)
     .click('#save-settings-button')
     .waitForElementVisible('#random-seed');
 }
