1d76c8b1489ca092e37386b507d71a7d1714c175
Karlis Zalite <karlis.zalite@gmail.com>
2020-09-18 13:17:31 +0200

    delete unused lines

--- app/ts/PreferencesTab/Preferences/PreferencesWeights/PreferencesWeights.tsx
+++ app/ts/PreferencesTab/Preferences/PreferencesWeights/PreferencesWeights.tsx
@@ -1,6 +1,6 @@
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 import {PreferencesContext} from '../../PreferencesContext';
 import PreferencesWeightsButtons from './PreferencesWeightsButtons/PreferencesWeightsButtons';
 import PreferencesWeightsTable from './PreferencesWeightsTable/PreferencesWeightsTable';
@@ -9,15 +9,6 @@ export default function PreferencesWeights() {
   const {determineElicitationMethod, pvfs, areAllPvfsSet} = useContext(
     PreferencesContext
   );
-  const [displayPvfWarning, setDisplayPvfWarning] = useState(
-    !areAllPvfsSet(pvfs)
-  );
-
-  useEffect(checkForPvfs, [pvfs]);
-
-  function checkForPvfs(): void {
-    setDisplayPvfWarning(!areAllPvfsSet(pvfs));
-  }
 
   return (
     <Grid item container>
