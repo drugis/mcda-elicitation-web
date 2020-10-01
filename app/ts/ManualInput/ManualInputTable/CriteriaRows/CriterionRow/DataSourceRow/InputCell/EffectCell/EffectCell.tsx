52ee19d5e85d1381895837dfbdddd93b8b262e03
Daan Reid <osmosisch@gmail.com>
2020-10-01 11:06:18 +0200

    Refactor effects display out of input cell; add enter for CI interval

--- app/ts/ManualInput/ManualInputTable/CriteriaRows/CriterionRow/DataSourceRow/InputCell/EffectCell/EffectCell.tsx
+++ app/ts/ManualInput/ManualInputTable/CriteriaRows/CriterionRow/DataSourceRow/InputCell/EffectCell/EffectCell.tsx
@@ -1,9 +1,8 @@
 import TableCell from '@material-ui/core/TableCell';
 import Tooltip from '@material-ui/core/Tooltip';
 import {Effect} from '@shared/interface/IEffect';
-import IRangeEffect from '@shared/interface/IRangeEffect';
-import IValueCIEffect from '@shared/interface/IValueCIEffect';
-import IValueEffect from '@shared/interface/IValueEffect';
+import IScale from '@shared/interface/IScale';
+import {renderEffect} from 'app/ts/EffectsTable/EffectsTableCriteriaRows/EffectsTableDataSourceRow/ValueCell/EffectValueCell/EffectValueCellService';
 import {ManualInputContext} from 'app/ts/ManualInput/ManualInputContext';
 import React, {useContext, useEffect, useState} from 'react';
 import {DataSourceRowContext} from '../../../DataSourceRowContext/DataSourceRowContext';
@@ -19,10 +18,15 @@ export default function EffectCell({alternativeId}: {alternativeId: string}) {
 
   const effect = getEffect(criterion.id, dataSource.id, alternativeId);
 
-  const INVALID_VALUE = 'Invalid value';
-
   useEffect(() => {
-    setLabel(createLabel(effect));
+    setLabel(
+      renderEffect(
+        effect,
+        'enteredData',
+        dataSource.unitOfMeasurement.type === 'percentage',
+        {} as IScale
+      )
+    );
   }, [effects, dataSource.unitOfMeasurement]);
 
   function saveEffect(effect: Effect) {
@@ -38,88 +42,13 @@ export default function EffectCell({alternativeId}: {alternativeId: string}) {
     setIsDialogOpen(false);
   }
 
-  function createLabel(effect: Effect): string {
-    switch (effect.type) {
-      case 'value':
-        return createValueLabel(effect);
-      case 'valueCI':
-        return createValueCILabel(effect);
-      case 'range':
-        return createRangeLabel(effect);
-      case 'text':
-        return effect.text ? effect.text : 'Empty';
-      case 'empty':
-        return 'Empty';
-    }
-  }
-
-  function valueIsOutofBounds(value: number): boolean {
-    return (
-      value < dataSource.unitOfMeasurement.lowerBound ||
-      value > dataSource.unitOfMeasurement.upperBound
-    );
-  }
-
-  function createValueLabel(effect: IValueEffect): string {
-    if (effect.value === undefined) {
-      return 'No value entered';
-    } else if (valueIsOutofBounds(effect.value)) {
-      return INVALID_VALUE;
-    } else if (dataSource.unitOfMeasurement.type === 'percentage') {
-      return `${effect.value}%`;
-    } else {
-      return `${effect.value}`;
-    }
-  }
-
-  function createValueCILabel(effect: IValueCIEffect): string {
-    if (hasOutOfBoundValue(effect)) {
-      return INVALID_VALUE;
-    } else if (dataSource.unitOfMeasurement.type === 'percentage') {
-      const lowerBound = effect.isNotEstimableLowerBound
-        ? 'NE'
-        : `${effect.lowerBound}%`;
-      const upperBound = effect.isNotEstimableUpperBound
-        ? 'NE'
-        : `${effect.upperBound}%`;
-      return `${effect.value}% (${lowerBound}, ${upperBound})`;
-    } else {
-      const lowerBound = effect.isNotEstimableLowerBound
-        ? 'NE'
-        : `${effect.lowerBound}`;
-      const upperBound = effect.isNotEstimableUpperBound
-        ? 'NE'
-        : `${effect.upperBound}`;
-      return `${effect.value} (${lowerBound}, ${upperBound})`;
-    }
-  }
-
-  function hasOutOfBoundValue(effect: IValueCIEffect): boolean {
-    return (
-      (!effect.isNotEstimableLowerBound &&
-        valueIsOutofBounds(effect.lowerBound)) ||
-      (!effect.isNotEstimableUpperBound &&
-        valueIsOutofBounds(effect.upperBound))
-    );
-  }
-
-  function createRangeLabel(effect: IRangeEffect): string {
-    if (
-      valueIsOutofBounds(effect.lowerBound) ||
-      valueIsOutofBounds(effect.upperBound)
-    ) {
-      return INVALID_VALUE;
-    } else if (dataSource.unitOfMeasurement.type === 'percentage') {
-      return `[${effect.lowerBound}%, ${effect.upperBound}%]`;
-    } else {
-      return `[${effect.lowerBound}, ${effect.upperBound}]`;
-    }
-  }
-
   return (
     <TableCell align="center">
       <Tooltip title="Edit effect">
-        <span onClick={openDialog} style={{cursor: 'pointer'}}>
+        <span
+          onClick={openDialog}
+          style={{cursor: 'pointer', whiteSpace: 'pre-wrap', minWidth: '6rem'}}
+        >
           {label}
         </span>
       </Tooltip>
