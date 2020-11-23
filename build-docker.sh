1c1c30375b0eaddcbcdb515bc117cfbaf5e67d56
Karlis Zalite <karlis.zalite@gmail.com>
2020-11-23 17:25:21 +0200

    update docker build script

--- build-docker.sh
+++ build-docker.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
 ATHENTICATION_METHOD="$1"
+MATOMO_VERSION="$2"
+
 if [ "$ATHENTICATION_METHOD" = "LOCAL" ]
 then
   WEBPACK_COMMAND="build-local-login"
 else 
   WEBPACK_COMMAND="build-prod"
 fi
+
 mkdir -p docker
 cp -Rf node-backend docker
 cp -Rf public docker
@@ -12,11 +16,15 @@ cp -Rf app docker
 cp -Rf examples docker
 cp -Rf schema docker
 cp -Rf ssl docker
+cp -Rf shared docker
+cp -Rf tutorials docker
 cp -f webpack* docker
 cp -f package.json docker
 cp -f Dockerfile docker
 cp -f yarn.lock docker
-cp -f index.js docker
+cp -f index.ts docker
+cp -f tsconfig.json docker
+cp -f ts-backend-config.json docker
 cd docker
 docker build --build-arg WEBPACK_COMMAND=$WEBPACK_COMMAND --build-arg MATOMO_VERSION=$MATOMO_VERSION --tag addis/mcda .
 cd ..
