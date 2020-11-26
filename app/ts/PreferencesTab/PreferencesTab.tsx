4c0d40327aad7b08b639f272c7d4b29492620dbc
Karlis Zalite <karlis.zalite@gmail.com>
2020-11-26 13:48:32 +0200

    add workspace and subproblem contexts, wip

--- app/ts/PreferencesTab/PreferencesTab.tsx
+++ app/ts/PreferencesTab/PreferencesTab.tsx
@@ -1,3 +1,6 @@
+import IOldSubproblem from '@shared/interface/IOldSubproblem';
+import IOldWorkspace from '@shared/interface/IOldWorkspace';
+import IScale from '@shared/interface/IScale';
 import ISettings from '@shared/interface/ISettings';
 import IToggledColumns from '@shared/interface/IToggledColumns';
 import IProblem from '@shared/interface/Problem/IProblem';
@@ -6,6 +9,8 @@ import React from 'react';
 import {ErrorContextProviderComponent} from '../Error/ErrorContext';
 import {HelpContextProviderComponent} from '../InlineHelp/HelpContext';
 import {SettingsContextProviderComponent} from '../Settings/SettingsContext';
+import {SubproblemContextProviderComponent} from '../Workspace/SubproblemContext/SubproblemContext';
+import {WorkspaceContextProviderComponent} from '../Workspace/WorkspaceContext';
 import Preferences from './Preferences/Preferences';
 import {PreferencesContextProviderComponent} from './PreferencesContext';
 
@@ -16,7 +21,11 @@ export default function PreferencesTab({
   problem,
   settings,
   updateAngularScenario,
-  toggledColumns
+  toggledColumns,
+  workspace,
+  scales,
+  subproblems,
+  currentSubproblem
 }: {
   scenarios: IMcdaScenario[];
   currentScenarioId: string;
@@ -25,24 +34,39 @@ export default function PreferencesTab({
   settings: ISettings;
   updateAngularScenario: (scenario: IMcdaScenario) => void;
   toggledColumns: IToggledColumns;
+  workspace: IOldWorkspace;
+  scales: Record<string, Record<string, IScale>>;
+  subproblems: IOldSubproblem[];
+  currentSubproblem: IOldSubproblem;
 }) {
   return scenarios && problem ? (
     <ErrorContextProviderComponent>
       <HelpContextProviderComponent>
-        <SettingsContextProviderComponent
-          settings={settings}
-          toggledColumns={toggledColumns}
+        <WorkspaceContextProviderComponent
+          oldWorkspace={workspace}
+          oldSubproblems={subproblems}
+          currentAngularSubproblem={currentSubproblem}
+          workspaceId={workspaceId}
+          subproblemChanged={() => {}}
+          scales={scales}
         >
-          <PreferencesContextProviderComponent
-            scenarios={scenarios}
-            currentScenarioId={currentScenarioId}
-            workspaceId={workspaceId}
-            problem={problem}
-            updateAngularScenario={updateAngularScenario}
-          >
-            <Preferences />
-          </PreferencesContextProviderComponent>
-        </SettingsContextProviderComponent>
+          <SubproblemContextProviderComponent>
+            <SettingsContextProviderComponent
+              settings={settings}
+              toggledColumns={toggledColumns}
+            >
+              <PreferencesContextProviderComponent
+                scenarios={scenarios}
+                currentScenarioId={currentScenarioId}
+                workspaceId={workspaceId}
+                problem={problem}
+                updateAngularScenario={updateAngularScenario}
+              >
+                <Preferences />
+              </PreferencesContextProviderComponent>
+            </SettingsContextProviderComponent>
+          </SubproblemContextProviderComponent>
+        </WorkspaceContextProviderComponent>
       </HelpContextProviderComponent>
     </ErrorContextProviderComponent>
   ) : (
