d608cce6b58f92312665659557c0a77943358828
Karlis Zalite <karlis.zalite@gmail.com>
2021-01-14 14:23:07 +0200

    add missing tests

--- app/ts/Legend/LegendWrapper/LegendUtil.test.ts
+++ app/ts/Legend/LegendWrapper/LegendUtil.test.ts
@@ -1,3 +1,21 @@
+import IAlternative from '@shared/interface/IAlternative';
+import {initLegend} from './LegendUtil';
+
 describe('LegendUtil', () => {
-  fail();
+  describe('initLegend', () => {
+    it('should return the existing legend', () => {
+      const legend: Record<string, string> = {alt1Id: 'Doge'};
+      const alternatives: IAlternative[] = [];
+      const result = initLegend(legend, alternatives);
+      expect(result).toEqual(legend);
+    });
+
+    it('should return alternative titles keyed by id', () => {
+      const legend: Record<string, string> = undefined;
+      const alternatives: IAlternative[] = [{id: 'alt1Id', title: 'alt1'}];
+      const result = initLegend(legend, alternatives);
+      const expectedResult: Record<string, string> = {alt1Id: 'alt1'};
+      expect(result).toEqual(expectedResult);
+    });
+  });
 });
