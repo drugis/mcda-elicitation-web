368839916f618c50b006251ede5a58715fd61706
Karlis Zalite <karlis.zalite@gmail.com>
2021-01-20 16:53:42 +0200

    add missing file

---
+++ app/ts/CriterionTooltip/CriterionTooltip.tsx
@@ -0,0 +1,20 @@
+import Tooltip from '@material-ui/core/Tooltip';
+import React from 'react';
+
+export default function CriterionTooltip({
+  title,
+  description
+}: {
+  title: string;
+  description: string;
+}): JSX.Element {
+  return (
+    <>
+      <Tooltip
+        title={description !== '' ? description : 'No description available'}
+      >
+        <span style={{color: '#0000ee'}}>{title}</span>
+      </Tooltip>
+    </>
+  );
+}
