6d8cd20121a3b396a73d16fe4a8df0a76635eefb
Joris <chinook25@gmail.com>
2021-01-22 12:01:16 +0100

    fix criterion tooltips

--- app/ts/CriterionTooltip/CriterionTooltip.tsx
+++ app/ts/CriterionTooltip/CriterionTooltip.tsx
@@ -10,9 +10,7 @@ export default function CriterionTooltip({
 }): JSX.Element {
   return (
     <>
-      <Tooltip
-        title={description !== '' ? description : 'No description available'}
-      >
+      <Tooltip title={description ? description : 'No description available'}>
         <span style={{color: '#0000ee'}}>{title}</span>
       </Tooltip>
     </>
