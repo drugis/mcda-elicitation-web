794abd5f48901b34e5a7636bb5fed30decdc4b28
Karlis Zalite <karlis.zalite@gmail.com>
2021-01-25 16:09:20 +0100

    extract component

--- app/ts/PreferencesTab/Preferences/TradeOff/TradeOffReferenceCriterionStatement/TradeOffReferenceCriterionStatement.tsx
+++ app/ts/PreferencesTab/Preferences/TradeOff/TradeOffReferenceCriterionStatement/TradeOffReferenceCriterionStatement.tsx
@@ -1,24 +1,18 @@
-import {Button, Grid, Popover, Slider} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import {
   canBePercentage,
   getPercentifiedValue
 } from 'app/ts/DisplayUtil/DisplayUtil';
-import {determineStepSize} from 'app/ts/PreferencesTab/Elicitation/MatchingElicitation/MatchingElicitationUtil';
 import {SettingsContext} from 'app/ts/Settings/SettingsContext';
 import _ from 'lodash';
 import React, {useContext, useState} from 'react';
 import {TradeOffContext} from '../TradeOffContext/TradeOffContext';
+import TradeOffSliderPopover from './TradeOffSliderPopover/TradeOffSliderPopover';
 
 export default function TradeOffReferenceCriterionStatement(): JSX.Element {
-  const {
-    lowerBound,
-    upperBound,
-    referenceValueFrom,
-    referenceValueTo,
-    setReferenceValueFrom,
-    setReferenceValueTo,
-    referenceCriterion
-  } = useContext(TradeOffContext);
+  const {referenceValueFrom, referenceValueTo, referenceCriterion} = useContext(
+    TradeOffContext
+  );
   const {showPercentages} = useContext(SettingsContext);
 
   const [isDialogOpen, setDialogOpen] = useState<boolean>(false);
@@ -26,11 +20,8 @@ export default function TradeOffReferenceCriterionStatement(): JSX.Element {
     null
   );
 
-  const stepSize = determineStepSize([lowerBound, upperBound]);
-
   const unit = referenceCriterion.dataSources[0].unitOfMeasurement.type;
   const usePercentage = showPercentages && canBePercentage(unit);
-  const isDecreasingPvf = referenceValueFrom > referenceValueTo;
 
   function openDialog(event: React.MouseEvent<HTMLButtonElement>): void {
     setDialogOpen(true);
@@ -40,21 +31,6 @@ export default function TradeOffReferenceCriterionStatement(): JSX.Element {
   function closeDialog(): void {
     setDialogOpen(false);
     setAnchorElement(null);
-  }
-
-  const marginTop = {marginTop: '50px'};
-
-  function handleSliderChanged(
-    event: React.ChangeEvent<any>,
-    newValue: [number, number]
-  ) {
-    if (isDecreasingPvf) {
-      setReferenceValueFrom(newValue[1]);
-      setReferenceValueTo(newValue[0]);
-    } else {
-      setReferenceValueFrom(newValue[0]);
-      setReferenceValueTo(newValue[1]);
-    }
   }
 
   return _.isNumber(referenceValueFrom) && _.isNumber(referenceValueTo) ? (
@@ -71,38 +47,11 @@ export default function TradeOffReferenceCriterionStatement(): JSX.Element {
         </Button>
         is equivalent to:
       </div>
-      <Popover
-        open={isDialogOpen}
-        onClose={closeDialog}
-        anchorEl={anchorElement}
-      >
-        <Grid container style={{minWidth: '400px', minHeight: '50px'}}>
-          <Grid item xs={2} style={{...marginTop, textAlign: 'center'}}>
-            {getPercentifiedValue(lowerBound, usePercentage)}
-          </Grid>
-          <Grid item xs={8} style={marginTop}>
-            <Slider
-              marks
-              valueLabelDisplay="on"
-              valueLabelFormat={(x: number) => {
-                return getPercentifiedValue(x, usePercentage);
-              }}
-              value={
-                isDecreasingPvf
-                  ? [referenceValueTo, referenceValueFrom]
-                  : [referenceValueFrom, referenceValueTo]
-              }
-              min={lowerBound}
-              max={upperBound}
-              onChange={handleSliderChanged}
-              step={stepSize}
-            />
-          </Grid>
-          <Grid item xs={2} style={{...marginTop, textAlign: 'center'}}>
-            {getPercentifiedValue(upperBound, usePercentage)}
-          </Grid>
-        </Grid>
-      </Popover>
+      <TradeOffSliderPopover
+        anchorElement={anchorElement}
+        isDialogOpen={isDialogOpen}
+        closeDialog={closeDialog}
+      />
     </>
   ) : (
     <></>
---
+++ app/ts/PreferencesTab/Preferences/TradeOff/TradeOffReferenceCriterionStatement/TradeOffSliderPopover/TradeOffSliderPopover.tsx
@@ -0,0 +1,82 @@
+import {Grid, Popover, Slider} from '@material-ui/core';
+import {
+  canBePercentage,
+  getPercentifiedValue
+} from 'app/ts/DisplayUtil/DisplayUtil';
+import {determineStepSize} from 'app/ts/PreferencesTab/Elicitation/MatchingElicitation/MatchingElicitationUtil';
+import {SettingsContext} from 'app/ts/Settings/SettingsContext';
+import React, {useContext} from 'react';
+import {TradeOffContext} from '../../TradeOffContext/TradeOffContext';
+
+export default function TradeOffSliderPopover({
+  anchorElement,
+  isDialogOpen,
+  closeDialog
+}: {
+  anchorElement: HTMLButtonElement;
+  closeDialog: () => void;
+  isDialogOpen: boolean;
+}): JSX.Element {
+  const {
+    lowerBound,
+    upperBound,
+    referenceValueFrom,
+    referenceValueTo,
+    setReferenceValueFrom,
+    setReferenceValueTo,
+    referenceCriterion
+  } = useContext(TradeOffContext);
+  const {showPercentages} = useContext(SettingsContext);
+
+  const marginTop = {marginTop: '50px'};
+
+  const stepSize = determineStepSize([lowerBound, upperBound]);
+
+  const unit = referenceCriterion.dataSources[0].unitOfMeasurement.type;
+  const usePercentage = showPercentages && canBePercentage(unit);
+  const isDecreasingPvf = referenceValueFrom > referenceValueTo;
+
+  function handleSliderChanged(
+    event: React.ChangeEvent<any>,
+    newValue: [number, number]
+  ) {
+    if (isDecreasingPvf) {
+      setReferenceValueFrom(newValue[1]);
+      setReferenceValueTo(newValue[0]);
+    } else {
+      setReferenceValueFrom(newValue[0]);
+      setReferenceValueTo(newValue[1]);
+    }
+  }
+
+  return (
+    <Popover open={isDialogOpen} onClose={closeDialog} anchorEl={anchorElement}>
+      <Grid container style={{minWidth: '400px', minHeight: '50px'}}>
+        <Grid item xs={2} style={{...marginTop, textAlign: 'center'}}>
+          {getPercentifiedValue(lowerBound, usePercentage)}
+        </Grid>
+        <Grid item xs={8} style={marginTop}>
+          <Slider
+            marks
+            valueLabelDisplay="on"
+            valueLabelFormat={(x: number) => {
+              return getPercentifiedValue(x, usePercentage);
+            }}
+            value={
+              isDecreasingPvf
+                ? [referenceValueTo, referenceValueFrom]
+                : [referenceValueFrom, referenceValueTo]
+            }
+            min={lowerBound}
+            max={upperBound}
+            onChange={handleSliderChanged}
+            step={stepSize}
+          />
+        </Grid>
+        <Grid item xs={2} style={{...marginTop, textAlign: 'center'}}>
+          {getPercentifiedValue(upperBound, usePercentage)}
+        </Grid>
+      </Grid>
+    </Popover>
+  );
+}
