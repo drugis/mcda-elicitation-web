ec260dad07ea12328e472880180b14dfce027e0b
Karlis Zalite <karlis.zalite@gmail.com>
2021-06-11 12:52:44 +0200

    hide favourability headers on problem definition tab if no criteria in that category

--- app/ts/EffectsTable/EffectsTableCriteriaRows/TableWithFavourability/TableWithFavourability.tsx
+++ app/ts/EffectsTable/EffectsTableCriteriaRows/TableWithFavourability/TableWithFavourability.tsx
@@ -1,6 +1,7 @@
 import TableBody from '@material-ui/core/TableBody';
 import ICriterion from '@shared/interface/ICriterion';
 import FavourabilityHeader from 'app/ts/EffectsTable/EffectsTableCriteriaRows/FavourabilityHeader/FavourabilityHeader';
+import ShowIf from 'app/ts/ShowIf/ShowIf';
 import _ from 'lodash';
 import React from 'react';
 
@@ -18,18 +19,22 @@ export default function TableWithFavourability({
 
   return (
     <TableBody>
-      <FavourabilityHeader
-        numberOfColumns={numberOfColumns}
-        headerId="favourable-criteria-label"
-        headerText="Favourable criteria"
-      />
-      {createCriteriaRows(favourableCriteria)}
-      <FavourabilityHeader
-        numberOfColumns={numberOfColumns}
-        headerId="unfavourable-criteria-label"
-        headerText="Unfavourable criteria"
-      />
-      {createCriteriaRows(unfavourableCriteria)}
+      <ShowIf condition={Boolean(favourableCriteria.length)}>
+        <FavourabilityHeader
+          numberOfColumns={numberOfColumns}
+          headerId="favourable-criteria-label"
+          headerText="Favourable criteria"
+        />
+        {createCriteriaRows(favourableCriteria)}
+      </ShowIf>
+      <ShowIf condition={Boolean(unfavourableCriteria.length)}>
+        <FavourabilityHeader
+          numberOfColumns={numberOfColumns}
+          headerId="unfavourable-criteria-label"
+          headerText="Unfavourable criteria"
+        />
+        {createCriteriaRows(unfavourableCriteria)}
+      </ShowIf>
     </TableBody>
   );
 }
