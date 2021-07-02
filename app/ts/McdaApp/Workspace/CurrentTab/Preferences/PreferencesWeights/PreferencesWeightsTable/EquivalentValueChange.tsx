fa5259b2aa49caddee330ddf803abb537dc1d877
Daan Reid <osmosisch@gmail.com>
2021-07-02 15:22:50 +0200

    recatoring, end-to-end test

---
+++ app/ts/McdaApp/Workspace/CurrentTab/Preferences/PreferencesWeights/PreferencesWeightsTable/EquivalentValueChange.tsx
@@ -0,0 +1,28 @@
+import {TPvf} from '@shared/interface/Problem/IPvf';
+import React, {useContext} from 'react';
+import {EquivalentChangeContext} from '../../EquivalentChange/EquivalentChangeContext/EquivalentChangeContext';
+import {getEquivalentValue} from '../../EquivalentChange/equivalentChangeUtil';
+
+export default function EquivalentValueChange({
+  usePercentage,
+  pvf,
+  otherWeight
+}: {
+  usePercentage: boolean;
+  pvf: TPvf;
+  otherWeight: number;
+}): JSX.Element {
+  const {referenceWeight, partOfInterval} = useContext(EquivalentChangeContext);
+
+  return (
+    <span>
+      {getEquivalentValue(
+        usePercentage,
+        otherWeight,
+        pvf,
+        partOfInterval,
+        referenceWeight
+      )}
+    </span>
+  );
+}
