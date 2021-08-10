e533420d1c258eeea4cb8465c59972d1e6f19066
Daan Reid <osmosisch@gmail.com>
2021-08-10 15:02:15 +0200

    fix configs

--- k8/init.sh
+++ k8/init.sh
@@ -1,11 +1,11 @@
-rancher kubectl create -f k8s/namespace.yaml #namespace creation works only through GUI for now
+# rancher kubectl create -f k8s/namespace.yaml #namespace creation works only through GUI for now
 
 rancher kubectl delete secret mcda-secrets -n mcda
 rancher kubectl create secret generic mcda-secrets \
   -n mcda \
   --from-literal=MCDAWEB_COOKIE_SECRET=GDFBDF#$%*asdfg098 \
-  --from-literal=MCDAWEB_GOOGLE_SECRET=Ju9grxy6LU72NlyHUUZ1xjDd \
-  --from-literal=MCDAWEB_GOOGLE_KEY=221368301791-7ark4468l8p2tt9sc5dvr9bb36si181h.apps.googleusercontent.com
+  --from-literal=MCDAWEB_GOOGLE_SECRET=WFU_VvlxrsyNLVUDkkTVgvfQ \
+  --from-literal=MCDAWEB_GOOGLE_KEY=290619536014-abnf3o5knc423o0n25939ql4ga0m0809.apps.googleusercontent.com
 
 rancher kubectl delete secret db-credentials -n mcda
 rancher kubectl create secret generic db-credentials \
@@ -19,11 +19,10 @@ rancher kubectl create configmap mcda-settings \
   -n mcda \
   --from-literal=MCDAWEB_AUTHENTICATION_METHOD=GOOGLE \
   --from-literal=MCDAWEB_DB_USER=mcda \
-  --from-literal=MCDA_HOST=mcda:3002 \
+  --from-literal=MCDA_HOST=https://mcda.edge.molgenis.org \
   --from-literal=MCDAWEB_DB_HOST=postgres \
   --from-literal=MCDAWEB_DB_NAME=mcda \
   --from-literal=PATAVI_HOST=patavi.edge.molgenis.org \
-  --from-literal=PATAVI_PORT=3000 \
   --from-literal=SECURE_TRAFFIC=true
 
 rancher kubectl delete secret passwords -n mcda
