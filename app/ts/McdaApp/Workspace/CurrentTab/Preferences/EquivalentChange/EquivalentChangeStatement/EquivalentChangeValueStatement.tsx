b644fce675d23c9e21e2d393b082871f06c4d779
chinook25 <chinook25@gmail.com>
2021-08-16 13:24:37 +0200

    change equivalent exchange input from slider to direct input

--- app/ts/McdaApp/Workspace/CurrentTab/Preferences/EquivalentChange/EquivalentChangeStatement/EquivalentChangeValueStatement.tsx
+++ app/ts/McdaApp/Workspace/CurrentTab/Preferences/EquivalentChange/EquivalentChangeStatement/EquivalentChangeValueStatement.tsx
@@ -1,9 +1,10 @@
 import {Button, Typography} from '@material-ui/core';
 import {getPercentifiedValue} from 'app/ts/DisplayUtil/DisplayUtil';
 import {SettingsContext} from 'app/ts/McdaApp/Workspace/SettingsContext/SettingsContext';
+import {getUnitLabel} from 'app/ts/util/getUnitLabel';
 import React, {useContext, useState} from 'react';
 import {EquivalentChangeContext} from '../EquivalentChangeContext/EquivalentChangeContext';
-import EquivalentChangeValueSlider from './EquivalentChangeSlider/EquivalentChangeValueslider';
+import EquivalentChangeValueInput from './EquivalentChangeInput/EquivalentChangeValueInput';
 
 export default function EquivalentChangeValueStatement() {
   const {referenceCriterion, referenceValueBy} = useContext(
@@ -14,7 +15,7 @@ export default function EquivalentChangeValueStatement() {
   const [isDialogOpen, setDialogOpen] = useState<boolean>(false);
   const usePercentage = getUsePercentage(referenceCriterion.dataSources[0]);
   const anchorElement = document.getElementById(
-    'reference-slider-by'
+    'reference-value-by'
   ) as HTMLButtonElement;
   function openDialog(event: React.MouseEvent<HTMLButtonElement>): void {
     setDialogOpen(true);
@@ -29,17 +30,13 @@ export default function EquivalentChangeValueStatement() {
     <>
       <Typography>
         The change of {referenceCriterion.title} by{' '}
-        <Button
-          id="reference-slider-by"
-          onClick={openDialog}
-          variant="outlined"
-        >
+        <Button id="reference-value-by" onClick={openDialog} variant="outlined">
           {getPercentifiedValue(referenceValueBy, usePercentage)}
         </Button>
-        {unit.label} is the basis for calculating the equivalent changes in the
-        table below.
+        {getUnitLabel(unit, usePercentage)} is the basis for calculating the
+        equivalent changes in the table below.
       </Typography>
-      <EquivalentChangeValueSlider
+      <EquivalentChangeValueInput
         anchorElement={anchorElement}
         isDialogOpen={isDialogOpen}
         closeDialog={closeDialog}
